fa97bf19126c4bfe60cd4faace958a87336f02fd
imjoseangel <josea.munoz@gmail.com>
2021-03-18 23:39:06 +0100

    Get starting combinations

---
+++ python/hackerrank/minion/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+
+from itertools import chain, combinations
+
+
+def minion_game(string):
+
+    vowels = ['A', 'E', 'I', 'O', 'U']
+    letters = list(string)
+
+    combines = list(chain.from_iterable(combinations(string, r)
+                                        for r in range(len(string) + 1)))
+
+    middleresult = [''.join(combine) for combine in combines if combine]
+    startcons = [item for item in middleresult if item[0]
+                 not in vowels]
+    startvowel = [item for item in middleresult if item[0] in vowels]
+
+
+def main():
+    # s = input()
+    s = 'BANANA'
+    minion_game(s)
+
+
+if __name__ == '__main__':
+    main()
