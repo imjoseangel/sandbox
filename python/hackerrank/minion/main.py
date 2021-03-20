94eb48bfd233ca6261fde13abc71d6dcd564a4e1
imjoseangel <josea.munoz@gmail.com>
2021-03-20 23:34:59 +0100

    got cons

--- python/hackerrank/minion/main.py
+++ python/hackerrank/minion/main.py
@@ -12,30 +12,30 @@ import re
 def minion_game(string):
 
     vowels = ['A', 'E', 'I', 'O', 'U']
-    finalcons = []
-    finalvowel = []
-    letters = list(string)
+    consnotwant = []
 
     combines = list(chain.from_iterable(combinations(string, r)
                                         for r in range(len(string) + 1)))
 
     middleresult = [''.join(combine) for combine in combines if combine]
-    startcons = [item for item in middleresult if item[0] not in vowels]
-    startvowel = [item for item in middleresult if item[0] in vowels]
+    startcons = list(
+        set([item for item in middleresult if item[0] not in vowels]))
+    startvowel = set([item for item in middleresult if item[0] in vowels])
 
-    for word in set(startcons):
-        if len(word) == 1:
-            finalcons.append(word)
-        else:
+    for word in startcons:
 
-            findcons = re.findall(r'[^aeiou]+',
-                                  word, re.IGNORECASE)
-            findvowel = re.findall(r'[aeiou]+',
-                                   word, re.IGNORECASE)
-            findword = findcons + findvowel
+        findcons = re.findall(r'[^aeiou]+',
+                              word, re.IGNORECASE)
+        findvowel = re.findall(r'[aeiou]+',
+                               word, re.IGNORECASE)
+        findword = findcons + findvowel
 
-            for item in findword:
-                print(word, len(item))
+        for notcons in findword:
+            if len(notcons) > 1:
+                consnotwant.append(word)
+
+    conslist = (list(set(startcons) - set(consnotwant)))
+    print(conslist)
 
 
 def main():
