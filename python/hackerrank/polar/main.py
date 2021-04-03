9cac58c7be22203942cd2aed9ec5932b5de0f0bd
imjoseangel <josea.munoz@gmail.com>
2021-04-03 23:48:19 +0200

    example of output

--- python/hackerrank/polar/main.py
+++ python/hackerrank/polar/main.py
@@ -3,10 +3,15 @@
 
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
+from cmath import phase
 
 
 def main():
-    pass
+
+    
+
+    print(abs(complex(1.0, 2.0)))
+    print(phase(complex(1.0, 2.0)))
 
 
 if __name__ == '__main__':
