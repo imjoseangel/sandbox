d9f69fc66bbe80dbd9110f22f1bb158aa31a8f4c
imjoseangel <josea.munoz@gmail.com>
2021-04-07 08:47:39 +0200

    add namedtuple

--- python/hackerrank/namedtuple/main.py
+++ python/hackerrank/namedtuple/main.py
@@ -4,9 +4,11 @@
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
+from collections import namedtuple
+
 
 def main():
-    pass
+    student = namedtuple('student', 'ID MARKS CLASS NAME')
 
 
 if __name__ == '__main__':
