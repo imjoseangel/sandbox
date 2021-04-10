963a528cad1a4a016d7b060f7053e004b3bfd579
imjoseangel <josea.munoz@gmail.com>
2021-04-10 23:38:00 +0200

    first approach read for now

--- python/hackerrank/timedelta/main.py
+++ python/hackerrank/timedelta/main.py
@@ -4,9 +4,17 @@
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
+import datetime
+
 
 def main():
-    pass
+
+    T = int(input().rstrip())
+
+    for _ in range(0, T * 2):
+        day, dd, mm, yyyy, time, diff = list(input().rstrip().split())
+
+        print(dd)
 
 
 if __name__ == '__main__':
