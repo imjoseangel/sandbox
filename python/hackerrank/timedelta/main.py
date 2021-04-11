533ee7bf8f565dd79d775c1b299aa79a6b1c64d2
imjoseangel <josea.munoz@gmail.com>
2021-04-11 19:11:03 +0200

    converting to datetime

--- python/hackerrank/timedelta/main.py
+++ python/hackerrank/timedelta/main.py
@@ -4,17 +4,26 @@
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
-import datetime
+from datetime import datetime
+from calendar import month_abbr
 
 
 def main():
 
     T = int(input().rstrip())
 
-    for _ in range(0, T * 2):
-        day, dd, mm, yyyy, time, diff = list(input().rstrip().split())
+    for _ in range(0, T):
+        day1, dd1, mm1, yyyy1, time1, diff1 = list(input().rstrip().split())
+        day2, dd2, mm2, yyyy2, time2, diff2 = list(input().rstrip().split())
 
-        print(dd)
+        x = datetime(year=int(yyyy1),
+                     month=(list(month_abbr).index(mm1)),
+                     day=int(dd1),
+                     hour=int(time1.split(":")[0]),
+                     minute=int(time1.split(":")[1]),
+                     second=int(time1.split(":")[2]))
+
+        print(x)
 
 
 if __name__ == '__main__':
