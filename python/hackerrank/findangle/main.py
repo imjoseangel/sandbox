846df56ba88369ae249d16a0ee2bc2546f5456cc
imjoseangel <josea.munoz@gmail.com>
2021-04-16 23:43:52 +0200

    first approach

--- python/hackerrank/findangle/main.py
+++ python/hackerrank/findangle/main.py
@@ -4,9 +4,26 @@
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
+from math import sqrt, pi, acos
+
 
 def main():
-    pass
+    AB = 10
+    c = 10
+    CA = sqrt(AB**2 + c**2)
+
+    b = CA / 2
+    a = sqrt(b**2 + c**2)
+
+    alpha = acos((b**2 + c**2 - a**2) / (2 * b * c))
+    betta = acos((a**2 + c**2 - b**2) / (2 * a * c))
+    gamma = acos((a**2 + b**2 - c**2) / (2 * a * b))
+
+    alpha = alpha * 180 / pi
+    betta = betta * 180 / pi
+    gamma = gamma * 180 / pi
+
+    print(alpha, betta, gamma)
 
 
 if __name__ == '__main__':
