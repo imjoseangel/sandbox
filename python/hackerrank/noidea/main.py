e41f6ceb822851f31991df654ab5ac42171ed124
imjoseangel <josea.munoz@gmail.com>
2021-05-09 18:12:34 +0200

    first approach

--- python/hackerrank/noidea/main.py
+++ python/hackerrank/noidea/main.py
@@ -6,7 +6,20 @@ from __future__ import (division, absolute_import, print_function,
 
 
 def main():
-    pass
+    m = 3
+    n = 2
+    array = [1, 5, 3]
+    a = (3, 1)
+    b = (5, 7)
+    initial = 0
+
+    for item in array:
+        if item in a:
+            initial += 1
+        elif item in b:
+            initial -= 1
+
+    print(initial)
 
 
 if __name__ == '__main__':
