f32e16825098f3d168b33a3d7e8793dd0b83039d
imjoseangel <josea.munoz@gmail.com>
2021-07-17 12:19:30 +0200

    final postalcode

--- python/hackerrank/postalcodes/main.py
+++ python/hackerrank/postalcodes/main.py
@@ -6,6 +6,7 @@ from __future__ import (division, absolute_import, print_function,
 import re
 
 regex_integer_in_range = r"^[1-9][0-9][0-9][0-9][0-9][0-9]$"
+regex_alternating_repetitive_digit_pair = r"(\d)(?=\d\1)"
 
 
 def main():
@@ -13,9 +14,10 @@ def main():
     try:
         P = input()
     except (ValueError, TypeError) as e:
-        print(e)
+        print(False)
 
-    print(bool(re.match(regex_integer_in_range, P)))
+    print(bool(re.match(regex_integer_in_range, P))
+          and len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2)
 
 
 if __name__ == '__main__':
