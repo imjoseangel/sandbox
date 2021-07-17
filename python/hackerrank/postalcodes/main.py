ffa8a5fa846646451a76b48c16bcb07b8e9a2fee
imjoseangel <josea.munoz@gmail.com>
2021-07-17 12:05:58 +0200

    basic input

--- python/hackerrank/postalcodes/main.py
+++ python/hackerrank/postalcodes/main.py
@@ -3,10 +3,18 @@
 
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
+import re
 
 
 def main():
-    pass
+
+    try:
+        p = int(input())
+    except (ValueError, TypeError) as e:
+        print(e)
+
+    if not 100000 <= p <= 999999:
+        print("False")
 
 
 if __name__ == '__main__':
