fe6b62f53ed6c4366f28e0c121cb06a0260d0619
imjoseangel <josea.munoz@gmail.com>
2021-07-25 19:51:31 +0200

    add clientcmd

--- go/kubernetes/main.go
+++ go/kubernetes/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"k8s.io/client-go/tools/clientcmd"
+)
+
 func main() {
-	// ...
+
+	kubeconfig := "~/.kube/config"
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 }
