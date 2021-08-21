0d93ad031af60f84c410a41bc78d4e4972796a73
imjoseangel <josea.munoz@gmail.com>
2021-08-21 22:40:10 +0200

    failing with negative numbers

--- python/hackerrank/2darrays/main.py
+++ python/hackerrank/2darrays/main.py
@@ -15,15 +15,15 @@ def main():
     items = [[arr[i][j:j + 3], arr[i + 1][j + 1:j + 2], arr[i + 2][j:j + 3]]
              for j in range(len(arr[0]) - 2) for i in range(len(arr) - 2)]
 
-    maxsum = 0
-
     for order in range(len(items)):
         total = 0
 
         for item in items[order]:
             total += sum(item)
 
-        if total > maxsum:
+        if order == 0:
+            maxsum = total
+        elif total > maxsum:
             maxsum = total
 
     print(maxsum)
