679cf74631a0582ac69ad04a335ed4c8b8e5f936
imjoseangel <josea.munoz@gmail.com>
2021-08-21 22:34:58 +0200

    improving first approach

--- python/hackerrank/2darrays/main.py
+++ python/hackerrank/2darrays/main.py
@@ -12,15 +12,13 @@ def main():
     for _ in range(6):
         arr.append(list(map(int, input().rstrip().split())))
 
-    items = [[arr[i][j:j + 3], arr[i + 1][j:j + 3], arr[i + 2][j:j + 3]]
+    items = [[arr[i][j:j + 3], arr[i + 1][j + 1:j + 2], arr[i + 2][j:j + 3]]
              for j in range(len(arr[0]) - 2) for i in range(len(arr) - 2)]
 
     maxsum = 0
 
     for order in range(len(items)):
         total = 0
-
-        del items[order][1][0], items[order][1][-1]
 
         for item in items[order]:
             total += sum(item)
