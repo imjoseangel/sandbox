f99d1639760095b2509c7682fa8a5dc3caeeb8de
imjoseangel <josea.munoz@gmail.com>
2021-08-21 20:09:32 +0200

    testing specific matrix

--- python/hackerrank/2darrays/main.py
+++ python/hackerrank/2darrays/main.py
@@ -7,10 +7,14 @@ from __future__ import (division, absolute_import, print_function,
 
 def main():
 
-    arr = []
+    arr = [[1, 1, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0],
+           [0, 0, 2, 4, 4, 0], [0, 0, 0, 2, 0, 0], [0, 0, 1, 2, 4, 0]]
 
-    for _ in range(6):
-        arr.append(list(map(int, input().rstrip().split())))
+    # for _ in range(6):
+    #     arr.append(list(map(int, input().rstrip().split())))
+
+    for i in range(3):
+        print(arr[i])
 
 
 if __name__ == '__main__':
