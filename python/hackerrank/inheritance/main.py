fa45590345bba30687a8fa64d044298d01bba36f
imjoseangel <josea.munoz@gmail.com>
2021-08-22 19:52:46 +0200

    add super

--- python/hackerrank/inheritance/main.py
+++ python/hackerrank/inheritance/main.py
@@ -5,6 +5,41 @@ from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
 
+class Person:
+
+    def __init__(self, firstName, lastName, idNumber):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.idNumber = idNumber
+
+    def printPerson(self):
+        print("Name:", self.lastName + ",", self.firstName)
+        print("ID:", self.idNumber)
+
+
+class Student(Person):
+
+    def __init__(self, firstName, lastName, idNumber, scores):
+
+        super().__init__(firstName, lastName, idNumber)
+        self.scores = scores
+
+    #   Class Constructor
+    #
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    # Write your constructor here
+
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    #
+    # Write your function here
+
+
 def main():
     pass
 
