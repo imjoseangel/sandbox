e980cb4ab9a9fd2a94a2c1c3d2dbac812b83ee7b
imjoseangel <josea.munoz@gmail.com>
2021-08-24 23:49:20 +0200

    first approach

---
+++ python/mobile/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+Builder.load_file('design.kv')
+
+
+class LoginScreen(Screen):
+    pass
+
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+
+
+if __name__ == '__main__':
+    MainApp().run()
