2ca074efbe9681aab37ac9ca376d22b3482016b0
imjoseangel <josea.munoz@gmail.com>
2021-08-25 20:22:14 +0200

    add user/pass

--- python/mobile/main.py
+++ python/mobile/main.py
@@ -4,9 +4,12 @@
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
+from datetime import datetime
+import json
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+
 
 Builder.load_file('design.kv')
 
@@ -18,7 +21,14 @@ class LoginScreen(Screen):
 
 class SignUpScreen(Screen):
     def add_user(self, uname, pword):
-        print(uname, pword)
+        with open('users.json') as file:
+            users = json.load(file)
+
+        users[uname] = {'username': uname, 'password': pword,
+                        'created': datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
+
+        with open('users.json', 'w') as file:
+            json.dump(users, file)
 
 
 class RootWidget(ScreenManager):
