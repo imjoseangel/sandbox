2b0d6b805ff4800aff9f4d56b0ae2fc58c83d4e7
imjoseangel <josea.munoz@gmail.com>
2021-08-29 20:44:28 +0200

    add final solution

--- python/hackerrank/linkedlist/main.py
+++ python/hackerrank/linkedlist/main.py
@@ -20,7 +20,14 @@ class Solution:
 
     def insert(self, head, data):
         # Complete this method
-        pass
+        if head is None:
+            head = Node(data)
+        else:
+            current = head
+            while current.next is not None:
+                current = current.next
+            current.next = Node(data)
+        return head
 
 
 def main():
