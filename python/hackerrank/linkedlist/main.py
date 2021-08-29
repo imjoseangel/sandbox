e5ad3f69096f389f47c6a3b93e625bac474d4559
imjoseangel <josea.munoz@gmail.com>
2021-08-29 20:40:22 +0200

    add basic

--- python/hackerrank/linkedlist/main.py
+++ python/hackerrank/linkedlist/main.py
@@ -5,8 +5,33 @@ from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Solution:
+    def display(self, head):
+        current = head
+        while current:
+            print(current.data, end=' ')
+            current = current.next
+
+    def insert(self, head, data):
+        # Complete this method
+        pass
+
+
 def main():
-    pass
+
+    mylist = Solution()
+    T = int(input())
+    head = None
+    for i in range(T):
+        data = int(input())
+        head = mylist.insert(head, data)
+    mylist.display(head)
 
 
 if __name__ == '__main__':
