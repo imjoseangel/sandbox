149b4b7826a82f54dcc37468a5085f17bddb06e7
imjoseangel <josea.munoz@gmail.com>
2021-09-04 23:44:23 +0200

    add basic

---
+++ python/hackerrank/binarysearch/main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+
+class Node:
+    def __init__(self, data):
+        self.right = self.left = None
+        self.data = data
+
+
+class Solution:
+    def insert(self, root, data):
+        if root == None:
+            return Node(data)
+        else:
+            if data <= root.data:
+                cur = self.insert(root.left, data)
+                root.left = cur
+            else:
+                cur = self.insert(root.right, data)
+                root.right = cur
+        return root
+
+    def getHeight(self, root):
+        pass
+
+
+def main():
+
+    T = int(input())
+    myTree = Solution()
+    root = None
+    for i in range(T):
+        data = int(input())
+        root = myTree.insert(root, data)
+    height = myTree.getHeight(root)
+    print(height)
+
+
+if __name__ == '__main__':
+    main()
