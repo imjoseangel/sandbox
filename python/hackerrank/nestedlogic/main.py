857418678e6f633a8f0d2821da7991568fe31b88
imjoseangel <josea.munoz@gmail.com>
2021-09-06 22:27:28 +0200

    add datetime

--- python/hackerrank/nestedlogic/main.py
+++ python/hackerrank/nestedlogic/main.py
@@ -4,9 +4,16 @@
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
+from datetime import date, datetime
+
 
 def main():
-    pass
+    rd, rm, ry = map(int, input().split())
+    dd, dm, dy = map(int, input().split())
+
+    datediff = datetime(ry, rm, rd) - datetime(dy, dm, dd)
+
+    print(datediff)
 
 
 if __name__ == '__main__':
