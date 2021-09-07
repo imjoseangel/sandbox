2e3b1b433a65624035fd73fd4bfeaa00da67b17a
imjoseangel <josea.munoz@gmail.com>
2021-09-07 12:13:12 +0200

    add bitwise

--- python/hackerrank/bitwiseand/main.py
+++ python/hackerrank/bitwiseand/main.py
@@ -4,9 +4,34 @@
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
+import math
+import os
+import random
+import re
+import sys
+
+
+def bitwiseAnd(N, K):
+    pass
+
 
 def main():
-    pass
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        count = int(first_multiple_input[0])
+
+        lim = int(first_multiple_input[1])
+
+        res = bitwiseAnd(count, lim)
+
+        fptr.write(str(res) + '\n')
+
+    fptr.close()
 
 
 if __name__ == '__main__':
