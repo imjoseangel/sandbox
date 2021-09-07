e8f46f7ab8eb338640a2a1c74775da4504b1e105
imjoseangel <josea.munoz@gmail.com>
2021-09-07 12:06:07 +0200

    first approach

--- python/hackerrank/regexpatterns/main.py
+++ python/hackerrank/regexpatterns/main.py
@@ -14,12 +14,21 @@ import sys
 def main():
     N = int(input().strip())
 
+    names = []
+
     for N_itr in range(N):
         first_multiple_input = input().rstrip().split()
 
         firstName = first_multiple_input[0]
 
         emailID = first_multiple_input[1]
+
+        if re.search(r'@gmail\.com$', emailID):
+
+            names.append(firstName)
+            names.sort()
+
+    print(*names, sep='\n')
 
 
 if __name__ == '__main__':
