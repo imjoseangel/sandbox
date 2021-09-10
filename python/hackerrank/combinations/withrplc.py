a934f4471f9fd8c0f646f14dd98e151e13b91539
imjoseangel <josea.munoz@gmail.com>
2021-09-10 16:15:00 +0200

    add with rplc

--- python/hackerrank/combinations/withrplc.py
+++ python/hackerrank/combinations/withrplc.py
@@ -4,9 +4,14 @@
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
+from itertools import combinations_with_replacement
+
 
 def main():
-    pass
+    s, n = input().strip().split()
+
+    for item in combinations_with_replacement(sorted(s), int(n)):
+        print(''.join(item))
 
 
 if __name__ == '__main__':
