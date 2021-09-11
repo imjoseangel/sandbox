4be7fa94078d4d5d555a962cd5e2b9a8277dd711
imjoseangel <josea.munoz@gmail.com>
2021-09-11 21:28:40 +0200

    add finalsol

--- python/hackerrank/disrmpop/main.py
+++ python/hackerrank/disrmpop/main.py
@@ -10,6 +10,19 @@ def main():
     n = int(input())
     s = set(map(int, input().split()))
 
+    x = int(input())
+
+    for _ in range(x):
+        command = input().split()
+        if command[0] == 'pop':
+            s.pop()
+        elif command[0] == 'remove':
+            s.remove(int(command[1]))
+        elif command[0] == 'discard':
+            s.discard(int(command[1]))
+
+    print(sum(s))
+
 
 if __name__ == '__main__':
     main()
