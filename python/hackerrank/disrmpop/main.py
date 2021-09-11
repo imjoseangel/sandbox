ef1a19adf47f966e05c93a32a87021c973becf8d
imjoseangel <josea.munoz@gmail.com>
2021-09-11 21:22:18 +0200

    add main

---
+++ python/hackerrank/disrmpop/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+
+def main():
+
+    n = int(input())
+    s = set(map(int, input().split()))
+
+
+if __name__ == '__main__':
+    main()
