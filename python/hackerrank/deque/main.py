9647fe0df667f47a97246fc2484563a4383ae691
imjoseangel <josea.munoz@gmail.com>
2021-09-12 11:42:33 +0200

    first steps

---
+++ python/hackerrank/deque/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+from collections import deque
+
+
+def main():
+    n = int(input())
+
+    q = []
+
+    for _ in range(n):
+        pass
+
+
+if __name__ == '__main__':
+    main()
