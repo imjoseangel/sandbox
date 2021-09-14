1cb5f770766dbddddf75673d21a0caf8a8cd4773
imjoseangel <josea.munoz@gmail.com>
2021-09-14 19:43:55 +0200

    add solution

---
+++ python/hackerrank/compressstring/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+from itertools import groupby
+
+
+def main():
+    s = input()
+    for k, g in groupby(s):
+        print(f'({len(list(g))}, {k})', end='')
+
+
+if __name__ == '__main__':
+    main()
