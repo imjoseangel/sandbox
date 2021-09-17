e8173853ac6eb5afd6eb0719a9469bf77e8899b1
imjoseangel <josea.munoz@gmail.com>
2021-09-17 18:44:11 +0200

    add base

--- python/hackerrank/triangle2/main.py
+++ python/hackerrank/triangle2/main.py
@@ -6,7 +6,9 @@ from __future__ import (division, absolute_import, print_function,
 
 
 def main():
-    pass
+    # More than 2 lines will result in 0 score. Do not leave a blank line also
+    for i in range(1, int(input()) + 1):
+        print
 
 
 if __name__ == '__main__':
