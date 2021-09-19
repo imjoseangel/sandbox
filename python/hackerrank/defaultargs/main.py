8a52a7a5a0f9033cf4cb469312257d25f1439cd4
imjoseangel <josea.munoz@gmail.com>
2021-09-19 20:23:18 +0200

    add main

---
+++ python/hackerrank/defaultargs/main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+
+class EvenStream(object):
+    def __init__(self):
+        self.current = 0
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+
+class OddStream(object):
+    def __init__(self):
+        self.current = 1
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+
+def print_from_stream(n, stream=EvenStream()):
+    for _ in range(n):
+        print(stream.get_next())
+
+
+def main():
+
+    queries = int(input())
+    for _ in range(queries):
+        stream_name, n = input().split()
+        n = int(n)
+        if stream_name == "even":
+            print_from_stream(n)
+        else:
+            print_from_stream(n, OddStream())
+
+
+if __name__ == '__main__':
+    main()
