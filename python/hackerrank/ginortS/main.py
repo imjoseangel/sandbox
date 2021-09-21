59b556981b4d71126768aaf0265bb590c1621120
imjoseangel <josea.munoz@gmail.com>
2021-09-21 10:09:43 +0200

    working solution

--- python/hackerrank/ginortS/main.py
+++ python/hackerrank/ginortS/main.py
@@ -13,7 +13,8 @@ def main():
     textupp = re.sub(r'[^A-Z]', '', n)
     numbers = re.sub(r'[a-zA-Z]', '', n)
 
-    print("".join(sorted(textlow)) + "".join(sorted(textupp)) + numbers)
+    print("".join(sorted(textlow)) +
+          "".join(sorted(textupp)) + "".join(sorted(numbers, key=lambda x: [int(x) % 2 == 0, x])))
 
 
 if __name__ == '__main__':
