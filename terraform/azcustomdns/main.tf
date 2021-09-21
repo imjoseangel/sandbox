b5d02f9c72e0bbfe5541be02bf07c055346fb0cd
imjoseangel <josea.munoz@gmail.com>
2021-09-21 13:05:41 +0200

    add example

---
+++ terraform/azcustomdns/main.tf
@@ -0,0 +1,65 @@
+provider "azurerm" {
+  alias           = "dns"
+  subscription_id = "09609272"
+  features {}
+}
+
+provider "azurerm" {
+  subscription_id = "94445108"
+  features {}
+}
+
+data "azurerm_dns_zone" "main" {
+  provider            = azurerm.dns
+  name                = "imjoseangel.eu"
+  resource_group_name = "we-s-rsg-publicdns"
+}
+
+data "azurerm_app_service" "main" {
+  name                = "dnsauto"
+  resource_group_name = "rsg-webapp"
+}
+
+resource "azurerm_dns_cname_record" "main" {
+  provider            = azurerm.dns
+  name                = data.azurerm_app_service.main.name
+  zone_name           = data.azurerm_dns_zone.main.name
+  resource_group_name = data.azurerm_dns_zone.main.resource_group_name
+  ttl                 = 3600
+  record              = format("%s.azurewebsites.net", data.azurerm_app_service.main.name)
+}
+
+resource "azurerm_dns_txt_record" "main" {
+  provider            = azurerm.dns
+  name                = format("asuid.%s", data.azurerm_app_service.main.name)
+  zone_name           = data.azurerm_dns_zone.main.name
+  resource_group_name = data.azurerm_dns_zone.main.resource_group_name
+  ttl                 = 3600
+
+  record {
+    value = data.azurerm_app_service.main.custom_domain_verification_id
+  }
+}
+
+resource "azurerm_app_service_custom_hostname_binding" "main" {
+  hostname            = trim(azurerm_dns_cname_record.main.fqdn, ".")
+  app_service_name    = data.azurerm_app_service.main.name
+  resource_group_name = data.azurerm_app_service.main.resource_group_name
+  depends_on          = [azurerm_dns_txt_record.main]
+
+  # Ignore ssl_state and thumbprint as they are managed using
+  # azurerm_app_service_certificate_binding
+  lifecycle {
+    ignore_changes = [ssl_state, thumbprint]
+  }
+}
+
+resource "azurerm_app_service_managed_certificate" "main" {
+  custom_hostname_binding_id = azurerm_app_service_custom_hostname_binding.main.id
+}
+
+resource "azurerm_app_service_certificate_binding" "main" {
+  hostname_binding_id = azurerm_app_service_custom_hostname_binding.main.id
+  certificate_id      = azurerm_app_service_managed_certificate.main.id
+  ssl_state           = "SniEnabled"
+}
