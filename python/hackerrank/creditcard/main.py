b5eb3396ce20263d37d75853f9ac33e747334ed2
imjoseangel <josea.munoz@gmail.com>
2021-09-26 18:48:26 +0200

    first approach

--- python/hackerrank/creditcard/main.py
+++ python/hackerrank/creditcard/main.py
@@ -4,9 +4,22 @@
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
+import re
+
 
 def main():
-    pass
+    pattern_match = r'^[456]\d{3}(-?\d{4}){3}$'
+    consecutive_digits = r'(\d)\1{3}'
+
+    n = int(input())
+
+    for _ in range(n):
+        credit_card = input()
+
+        if re.match(pattern_match, credit_card) and not re.search(consecutive_digits, credit_card):
+            print('Valid')
+        else:
+            print('Invalid')
 
 
 if __name__ == '__main__':
