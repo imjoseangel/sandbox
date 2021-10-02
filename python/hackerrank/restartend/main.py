76554e7efbd3753fbc43f27ca4e8d2ef983ed864
imjoseangel <josea.munoz@gmail.com>
2021-10-02 18:18:25 +0200

    first test

--- python/hackerrank/restartend/main.py
+++ python/hackerrank/restartend/main.py
@@ -4,9 +4,16 @@
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
+import re
+
 
 def main():
-    pass
+    S = str(input())
+    k = str(input())
+
+    m = re.search(k, S)
+
+    print(m.end(), m.start())
 
 
 if __name__ == '__main__':
