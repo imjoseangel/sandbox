efc8cad45c8d643a1d12b347cb58f1e1c4ae8467
imjoseangel <josea.munoz@gmail.com>
2021-10-06 22:16:50 +0200

    add sum of pows

--- python/hackerrank/integesallsizes/main.py
+++ python/hackerrank/integesallsizes/main.py
@@ -6,7 +6,9 @@ from __future__ import (division, absolute_import, print_function,
 
 
 def main():
-    pass
+    a, b, c, d = (int(input()) for _ in range(4))
+
+    print(pow(a, b) + pow(c, d))
 
 
 if __name__ == '__main__':
