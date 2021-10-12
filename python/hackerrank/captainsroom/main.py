6b78709bb1b19ef10d65ee5f834476909b0b1eac
imjoseangel <josea.munoz@gmail.com>
2021-10-12 10:20:11 +0200

    add base

---
+++ python/hackerrank/captainsroom/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+
+def main():
+
+    K = int(input())
+    n = map(int, input().split())
+
+    n.sort()
+
+
+if __name__ == '__main__':
+    main()
