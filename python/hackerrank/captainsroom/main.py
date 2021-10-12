5183a92ea3d7d521e1b93192518d3704ebdf29a9
imjoseangel <josea.munoz@gmail.com>
2021-10-12 10:25:04 +0200

    final sol

--- python/hackerrank/captainsroom/main.py
+++ python/hackerrank/captainsroom/main.py
@@ -4,13 +4,16 @@
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
+from collections import Counter
+
 
 def main():
 
-    K = int(input())
-    n = map(int, input().split())
+    _ = int(input())
+    n = [item for item, count in Counter(
+        list(map(int, input().split()))).items() if count == 1]
 
-    n.sort()
+    print(n[0])
 
 
 if __name__ == '__main__':
