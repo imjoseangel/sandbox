5309a8587a5d088e57ff5d886d5c79786b9eb378
imjoseangel <josea.munoz@gmail.com>
2021-10-16 22:53:39 +0200

    add basics

---
+++ python/hackerrank/zipped/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+
+def main():
+    X, Y = (int(input()) for _ in range(2))
+
+
+if __name__ == '__main__':
+    main()
