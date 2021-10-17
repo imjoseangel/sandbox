dd6846060805cb5940b1623f6b0d07fcc75ae4ba
imjoseangel <josea.munoz@gmail.com>
2021-10-17 20:09:19 +0200

    add input

---
+++ python/hackerrank/input/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+
+def main():
+    _, y = list(map(int, input().split()))
+
+    print(eval(input()) == y)
+
+
+if __name__ == '__main__':
+    main()
