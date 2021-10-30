30b7e83be0c74c986521a3aade65ec65d5bb40a7
imjoseangel <josea.munoz@gmail.com>
2021-10-30 12:55:29 +0200

    add eval M1

---
+++ python/hackerrank/evaluation/pyeval.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+
+def main():
+
+    eval(str(input()))
+
+
+if __name__ == '__main__':
+    main()
