a8c1f3b70ce5d6caf77fd0e7ffccf5713f2fe850
imjoseangel <josea.munoz@gmail.com>
2021-11-01 18:19:52 +0100

    code without testing

--- python/blockchain/main.py
+++ python/blockchain/main.py
@@ -19,10 +19,17 @@ class Block:
     def getHash(self):
         return sha256(self.prevHash + self.timestamp + json(self.data)).hexdigest()
 
+    def mine(self, difficulty):
+        while self.hash[:difficulty] != "0" * difficulty:
+            self.nonce += 1
+            self.hash = self.getHash()
+
 
 class Blockchain:
     def __init__(self):
         self.chain = [Block(str(datetime.now()))]
+        self.difficulty = 1
+        self.blockTime = 1000
 
     def getLastBlock(self):
         return self.chain[-1]
@@ -30,11 +37,26 @@ class Blockchain:
     def addBlock(self, block):
         block.prevHash = self.getLastBlock().hash
         block.hash = block.getHash()
+        block.mine(self.difficulty)
         self.chain.append(block)
+        miningTime = datetime.now() - int(self.getLastBlock().timestamp)
+        self.difficulty = round(
+            self.difficulty * self.blockTime / (miningTime, 1)[miningTime <= 0])
+
+    def isValid(self):
+        for i in range(1, len(self.chain)):
+            currentBlock = self.chain[i]
+            prevBlock = self.chain[i - 1]
+
+            if currentBlock.hash != currentBlock.getHash() or prevBlock.hash != currentBlock.prevHash:
+                return False
+
+        return True
 
 
 def main():
-    pass
+    JeChain = Blockchain()
+    print(JeChain)
 
 
 if __name__ == '__main__':
