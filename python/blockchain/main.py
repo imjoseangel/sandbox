19277aa768cac7f0772e64169818334dc9dfa2ec
imjoseangel <josea.munoz@gmail.com>
2021-11-01 11:00:17 +0100

    add nontested code

--- python/blockchain/main.py
+++ python/blockchain/main.py
@@ -4,11 +4,33 @@
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
+from hashlib import sha256
+import json
+from datetime import datetime
+
 
 class Block:
     def __init__(self, timestamp="", data=[]):
         self.timestamp = timestamp
         self.data = data
+        self.hash = self.getHash()
+        self.prevHash = ""
+
+    def getHash(self):
+        return sha256(self.prevHash + self.timestamp + json(self.data)).hexdigest()
+
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [Block(str(datetime.now()))]
+
+    def getLastBlock(self):
+        return self.chain[-1]
+
+    def addBlock(self, block):
+        block.prevHash = self.getLastBlock().hash
+        block.hash = block.getHash()
+        self.chain.append(block)
 
 
 def main():
