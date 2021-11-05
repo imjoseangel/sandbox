0ff11338e982e811be316d5e2eef54ae0a937e8a
imjoseangel <josea.munoz@gmail.com>
2021-11-05 22:20:50 +0100

    add first approach

--- python/hackerrank/romannumerals/main.py
+++ python/hackerrank/romannumerals/main.py
@@ -4,9 +4,12 @@
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
+import re
+
 
 def main():
-    pass
+    regex_pattern = r"([MDCLXVI]+\b)"
+    print(str(bool(re.match(regex_pattern, input()))))
 
 
 if __name__ == '__main__':
