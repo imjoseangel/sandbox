814f7a6f82d1141c0d4754f951c3c83b420d410b
imjoseangel <josea.munoz@gmail.com>
2021-11-05 22:22:38 +0100

    good approach

--- python/hackerrank/romannumerals/main.py
+++ python/hackerrank/romannumerals/main.py
@@ -8,7 +8,7 @@ import re
 
 
 def main():
-    regex_pattern = r"([MDCLXVI]+\b)"
+    regex_pattern = r"^(?=[MDCLXVI])M*(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$"
     print(str(bool(re.match(regex_pattern, input()))))
 
 
