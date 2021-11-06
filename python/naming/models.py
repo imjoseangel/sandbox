57339351cfb819a5b9f3cec2ed04289dc2bcd6b3
imjoseangel <josea.munoz@gmail.com>
2021-11-07 00:35:53 +0100

    add name as pl

--- python/naming/models.py
+++ python/naming/models.py
@@ -7,16 +7,15 @@ from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
 class Naming(db.Model):
     __tablename__ = "resourceDefinition"
-    index = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True, nullable=False)
+    index = db.Column(db.Integer)
+    name = db.Column(db.String(64), primary_key=True,
+                     unique=True, nullable=False)
     regex = db.Column(db.String(256))
     scope = db.Column(db.String(16))
     slug = db.Column(db.String(16))
     dashes = db.Column(db.Boolean())
     lengthmin = db.Column(db.Integer())
     lengthmax = db.Column(db.Integer())
-    timestamp = db.Column(
-        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
 
 class NamingSchema(SQLAlchemyAutoSchema):
