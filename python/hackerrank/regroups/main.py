ca6add7f9cdc679cac019e757a741fd667f8e4f1
imjoseangel <josea.munoz@gmail.com>
2021-11-09 20:18:07 +0100

    first approach

--- python/hackerrank/regroups/main.py
+++ python/hackerrank/regroups/main.py
@@ -4,9 +4,14 @@
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
+import re
+
 
 def main():
-    pass
+    S = input()
+
+    result = re.search(r'(\w(?!_))\1+', S)
+    print(result.group(1) if result else -1)
 
 
 if __name__ == '__main__':
