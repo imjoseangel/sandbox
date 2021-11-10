fc25e99b58758581e8f236081a4a1da16962d11f
imjoseangel <josea.munoz@gmail.com>
2021-11-10 22:42:11 +0100

    add main

---
+++ python/bankpython/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+import barbara
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
