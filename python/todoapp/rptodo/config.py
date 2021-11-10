e2e1b6342963f3ebff5e4dd56d5504b8a7a6b131
imjoseangel <josea.munoz@gmail.com>
2021-11-10 23:23:43 +0100

    halfway

--- python/todoapp/rptodo/config.py
+++ python/todoapp/rptodo/config.py
@@ -8,7 +8,7 @@ from pathlib import Path
 import typer
 
 from rptodo import (
-    DB_WRITE_ERROR, DIR_ERROR, FILE_ERROR, SUCCESS, __app_name_
+    DB_WRITE_ERROR, DIR_ERROR, FILE_ERROR, SUCCESS, __app_name__
 )
 
 CONFIG_DIR_PATH = Path(typer.get_app_dir(__app_name__))
