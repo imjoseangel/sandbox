e0a32f932a399f419a3bdb08205d783a03f87d72
imjoseangel <josea.munoz@gmail.com>
2021-11-12 21:13:40 +0100

    working tests

--- python/todoapp/rptodo/rptodo.py
+++ python/todoapp/rptodo/rptodo.py
@@ -3,8 +3,9 @@
 # -*- coding: utf-8 -*-
 
 from pathlib import Path
-from typing import Any, Dict, NamedTuple
+from typing import Any, Dict, List, NamedTuple
 
+from rptodo import DB_READ_ERROR
 from rptodo.database import DatabaseHandler
 
 
@@ -16,3 +17,20 @@ class CurrentTodo(NamedTuple):
 class Todoer:
     def __init__(self, db_path: Path) -> None:
         self._db_handler = DatabaseHandler(db_path)
+
+    def add(self, description: List[str], priority: int = 2) -> CurrentTodo:
+        """Add a new to-do to the database."""
+        description_text = " ".join(description)
+        if not description_text.endswith("."):
+            description_text += "."
+        todo = {
+            "Description": description_text,
+            "Priority": priority,
+            "Done": False,
+        }
+        read = self._db_handler.read_todos()
+        if read.error == DB_READ_ERROR:
+            return CurrentTodo(todo, read.error)
+        read.todo_list.append(todo)
+        write = self._db_handler.write_todos(read.todo_list)
+        return CurrentTodo(todo, write.error)
