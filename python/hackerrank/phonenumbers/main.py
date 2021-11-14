2133fc651bc5ad2b463ede8926c8456cae799d6b
imjoseangel <josea.munoz@gmail.com>
2021-11-14 16:30:02 +0100

    add with lint

--- python/hackerrank/phonenumbers/main.py
+++ python/hackerrank/phonenumbers/main.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+Pattern for phone number.
+"""
+
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
+import re
+
+PATTERN = r'^[7-9]\d{9}$'
+
 
 def main():
+    """
+    Main function.
+    """
 
     for _ in range(int(input())):
         phone_number = input()
-        print(phone_number)
+        if re.match(PATTERN, phone_number):
+            print('YES')
+        else:
+            print('NO')
 
 
 if __name__ == '__main__':
