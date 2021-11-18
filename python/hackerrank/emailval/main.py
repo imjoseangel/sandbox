f0623d6494519367eb6296874ca63c2cd7313857
imjoseangel <josea.munoz@gmail.com>
2021-11-18 21:03:08 +0100

    lets have dinner

--- python/hackerrank/emailval/main.py
+++ python/hackerrank/emailval/main.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+Validate email addresses
+"""
+
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
+import email.utils
+
 
 def main():
-    pass
+    """
+    Main function
+    """
+
+    for _ in range(int(input())):
+        print(email.utils.parseaddr(input())[1])
 
 
 if __name__ == '__main__':
