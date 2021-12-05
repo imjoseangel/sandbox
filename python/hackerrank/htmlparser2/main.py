69cd06931ccf74db0844b5f10682957ec5c2a812
imjoseangel <josea.munoz@gmail.com>
2021-12-05 22:52:29 +0100

    add solution

--- python/hackerrank/htmlparser2/main.py
+++ python/hackerrank/htmlparser2/main.py
@@ -13,6 +13,26 @@ from html.parser import HTMLParser
 class MyHTMLParser(HTMLParser):
     """ HTML Parser """
 
+    def handle_comment(self, data):
+        """
+        :param data:
+        :return:
+        """
+        if '\n' in data:
+            print('>>> Multi-line Comment')
+        else:
+            print('>>> Single-line Comment')
+        print(data)
+
+    def handle_data(self, data):
+        """
+        :param data:
+        :return:
+        """
+        if data.strip():
+            print('>>> Data')
+            print(data)
+
 
 def main():
     """
