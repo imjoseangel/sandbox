7448ff970dd7faf1a5c0002954b0922fadc7f10e
imjoseangel <josea.munoz@gmail.com>
2021-12-05 21:12:30 +0100

    add main

---
+++ python/hackerrank/htmlparser2/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Python Template
+"""
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+from html.parser import HTMLParser
+
+
+class MyHTMLParser(HTMLParser):
+    """ HTML Parser """
+    pass
+
+
+def main():
+    """
+    Main function
+    """
+
+    html = ""
+    for _ in range(int(input())):
+        html += input().rstrip()
+        html += '\n'
+
+    parser = MyHTMLParser()
+    parser.feed(html)
+    parser.close()
+
+
+if __name__ == '__main__':
+    main()
