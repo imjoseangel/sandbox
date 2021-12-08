bdb6ac3e5cec9149ed1e92a99f254c6dbbeb86c2
imjoseangel <josea.munoz@gmail.com>
2021-12-08 19:47:16 +0100

    add test

--- python/resolver/customresolver.py
+++ python/resolver/customresolver.py
@@ -7,7 +7,7 @@ Python Template
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 import requests
-import etchosts
+import dnsmock
 
 
 def main():
@@ -15,7 +15,7 @@ def main():
     Main function
     """
 
-    etchosts.bind_ip('www.example.com', 443, '216.58.215.174')
+    dnsmock.bind_ip('www.example.com', 443, '127.0.0.1')
 
     # this sends requests to 216.58.215.174
     response = requests.get('https://www.example.com', verify=True)
