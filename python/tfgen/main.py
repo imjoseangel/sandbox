3ee0312cb288bff72f3792d12607298528b18127
imjoseangel <josea.munoz@gmail.com>
2021-12-12 14:32:33 +0100

    linting pylint

--- python/tfgen/main.py
+++ python/tfgen/main.py
@@ -12,10 +12,10 @@ import re
 from jinja2 import FileSystemLoader, Environment
 
 
-def cleanRegex(input):
+def cleanregex(jinput: str) -> str:
     """Custom filter"""
     pattern = '(?m)\\(\\?=.{\\d+,\\d+}\\$\\)|\\(\\?!\\.\\*--\\)'
-    return re.sub(re.compile(pattern), "", input)
+    return re.sub(re.compile(pattern), "", jinput)
 
 
 def main():
@@ -23,7 +23,7 @@ def main():
     Main function
     """
 
-    with open('resourceDefinition.json') as json_file:
+    with open('resourceDefinition.json', encoding='utf-8') as json_file:
         data = json.load(json_file)
 
     # with open('templates/main.j2', 'r', encoding="utf-8") as open_file:
@@ -31,14 +31,14 @@ def main():
     loader = FileSystemLoader('templates')
     env = Environment(autoescape=True, loader=loader)
 
-    env.filters['cleanRegex'] = cleanRegex
+    env.filters['cleanRegex'] = cleanregex
     temp = env.get_template('main.j2')
     maintemplate = temp.render(data=data)
 
-    f = open("main.tf", "w", encoding="utf-8")
-    f.write(maintemplate)
-    f.write("\n")
-    f.close()
+    with open("main.tf", "w", encoding="utf-8") as maintf:
+        maintf.write(maintemplate)
+        maintf.write("\n")
+        maintf.close()
 
 
 if __name__ == '__main__':
