beb7296a116e03c9ef98a14124f15a51830b86dc
imjoseangel <josea.munoz@gmail.com>
2022-01-01 12:03:49 +0100

    add asserts

---
+++ python/hackerrank/uidval/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Python Template
+"""
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+import re
+
+
+def main():
+    """
+    Main function
+    """
+    for _ in range(int(input())):
+        item = ''.join(sorted(input()))
+        try:
+            assert re.search(r'[A-Z]{2}', item)
+            assert re.search(r'[0-9]{3}', item)
+            assert not re.search(r'[^a-zA-Z0-9]', item)
+            assert not re.search(r'(.)\1', item)
+            assert len(item) == 10
+        except:
+            print('Invalid')
+        else:
+            print('Valid')
+
+
+if __name__ == '__main__':
+    main()
