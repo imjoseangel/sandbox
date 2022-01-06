685d6b496b67faa6cf6d327a42223b51804121cf
imjoseangel <josea.munoz@gmail.com>
2022-01-06 10:32:44 +0100

    add basic

---
+++ python/eye/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Python Template
+"""
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+import numpy as np
+
+
+def main():
+    """
+    Main function
+    """
+
+
+if __name__ == '__main__':
+    main()
