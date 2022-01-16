319a2e1be745bf575d4dda5dbfaf43544a6f3357
imjoseangel <josea.munoz@gmail.com>
2022-01-16 10:13:06 +0100

    add result

--- python/hackerrank/sumprod/main.py
+++ python/hackerrank/sumprod/main.py
@@ -7,11 +7,17 @@ Python Template
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
+import numpy
+
 
 def main():
     """
     Main function
     """
+
+    N, _ = map(int, input().split())
+    my_array = numpy.array([input().split() for _ in range(N)], int)
+    print(numpy.array(my_array.sum(axis=0)).prod())
 
 
 if __name__ == '__main__':
