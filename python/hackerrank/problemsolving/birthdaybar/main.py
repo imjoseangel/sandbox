436bc9859e39777e3acb385c51956f5e4b316617
imjoseangel <josea.munoz@gmail.com>
2022-01-19 22:06:31 +0100

    preparation

---
+++ python/hackerrank/problemsolving/birthdaybar/main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Python Template
+"""
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+#
+
+
+def birthday(s, d, m):
+    # Write your code here
+    pass
+
+
+def main():
+    """
+    Main function
+    """
+
+    n = int(input().strip())
+    s = list(map(int, input().rstrip().split()))
+
+    first_multiple_input = input().rstrip().split()
+
+    d = int(first_multiple_input[0])
+    m = int(first_multiple_input[1])
+
+    result = birthday(s, d, m)
+
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
