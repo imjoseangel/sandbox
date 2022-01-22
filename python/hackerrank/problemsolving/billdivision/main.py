be7c9fa167a5f2714fc260c45c59599c81491a13
imjoseangel <josea.munoz@gmail.com>
2022-01-22 23:22:45 +0100

    add main

---
+++ python/hackerrank/problemsolving/billdivision/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Python Template
+"""
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+
+def main():
+    """
+    Main function
+    """
+
+
+if __name__ == '__main__':
+    main()
