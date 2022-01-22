58b47092dcb4f722357621a1906afdb2c01fe431
imjoseangel <josea.munoz@gmail.com>
2022-01-22 23:24:53 +0100

    first try

--- python/hackerrank/problemsolving/billdivision/main.py
+++ python/hackerrank/problemsolving/billdivision/main.py
@@ -8,10 +8,41 @@ from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
 
+def bonAppetit(bill, k, b):
+    """
+    Function to calculate the amount of money that Anna did not share with
+    Bob.
+    """
+    # Calculate the total amount of money that Anna paid for all the items
+    # that Bob did not share.
+    anna_paid = sum(bill[:k]) - bill[k]
+
+    # Calculate the amount of money that Anna did not share with Bob.
+    anna_did_not_share = anna_paid / 2
+
+    # Compare the amount of money that Anna did not share with Bob with the
+    # amount of money that Bob paid to Anna.
+    if anna_did_not_share == b:
+        print('Bon Appetit')
+    else:
+        print(int(b - anna_did_not_share))
+
+
 def main():
     """
     Main function
     """
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
 
 
 if __name__ == '__main__':
