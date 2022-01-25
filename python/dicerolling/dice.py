7497b8d779ed3b0c1828c77289ed0bf9a10f60f3
imjoseangel <josea.munoz@gmail.com>
2022-01-25 22:06:29 +0100

    add parser

--- python/dicerolling/dice.py
+++ python/dicerolling/dice.py
@@ -5,6 +5,20 @@ from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
 
+def parse_input(input_string):
+    """Return `input_string` as an integer between 1 and 6.
+
+    Check if `input_string` is an integer number between 1 and 6.
+    If so, return an integer with the same value. Otherwise, tell
+    the user to enter a valid number and quit the program.
+    """
+    if input_string.strip() in {"1", "2", "3", "4", "5", "6"}:
+        return int(input_string)
+    else:
+        print("Please enter a number from 1 to 6.")
+        raise SystemExit(1)
+
+
 def main():
     """
     ~~~ App's main code block ~~~
