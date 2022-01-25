56e2585dad07bb47718d17e6fb9b66768ea26a98
imjoseangel <josea.munoz@gmail.com>
2022-01-25 22:49:53 +0100

    final approach

--- python/dicerolling/dice.py
+++ python/dicerolling/dice.py
@@ -97,19 +97,8 @@ def generate_dice_faces_diagram(dice_values):
     │  ●   ●  │ │         │ │      ●  │ │      ●  │
     └─────────┘ └─────────┘ └─────────┘ └─────────┘
     """
-    # Generate a list of dice faces from DICE_ART
-    dice_faces = []
-    for value in dice_values:
-        dice_faces.append(DICE_ART[value])
-
-    # Generate a list containing the dice faces rows
-    dice_faces_rows = []
-    for row_idx in range(DIE_HEIGHT):
-        row_components = []
-        for dice in dice_faces:
-            row_components.append(dice[row_idx])
-        row_string = DIE_FACE_SEPARATOR.join(row_components)
-        dice_faces_rows.append(row_string)
+    dice_faces = _get_dice_faces(dice_values)
+    dice_faces_rows = _generate_dice_faces_rows(dice_faces)
 
     # Generate header with the word "RESULTS" centered
     width = len(dice_faces_rows[0])
@@ -117,6 +106,24 @@ def generate_dice_faces_diagram(dice_values):
 
     dice_faces_diagram = "\n".join([diagram_header] + dice_faces_rows)
     return dice_faces_diagram
+
+
+def _get_dice_faces(dice_values):
+    dice_faces = []
+    for value in dice_values:
+        dice_faces.append(DICE_ART[value])
+    return dice_faces
+
+
+def _generate_dice_faces_rows(dice_faces):
+    dice_faces_rows = []
+    for row_idx in range(DIE_HEIGHT):
+        row_components = []
+        for die in dice_faces:
+            row_components.append(die[row_idx])
+        row_string = DIE_FACE_SEPARATOR.join(row_components)
+        dice_faces_rows.append(row_string)
+    return dice_faces_rows
 
 
 def main():
