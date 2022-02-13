609b0f751418e28be28dd891773df44cfc50c648
imjoseangel <josea.munoz@gmail.com>
2022-02-13 12:16:41 +0100

    add macsec

--- python/networking/main.py
+++ python/networking/main.py
@@ -3,9 +3,13 @@
 
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
+from prometheus_client import Summary
 
-from scapy.all import Ether, IP, TCP
+import scapy.all as scapy
+from scapy.layers.inet import Ether
 from scapy.contrib.macsec import MACsec
+from scapy.layers.inet import IP, TCP
+from scapy.all import *
 
 
 def main():
@@ -15,6 +19,14 @@ def main():
     p = Ether() / IP(dst="www.secdev.org") / TCP()
     print(p.summary())
     print(p.show())
+
+    data = 'test_MACsec'
+
+    pkt = Ether(dst="00:00:01:00:00:01",
+                src="00:10:94:00:00:02") / MACsec() / data
+    sendp(pkt, iface="eth2", count=400)
+    print(pkt.summary())
+    print(pkt.show())
 
 
 if __name__ == '__main__':
