7e9922e59e6877840f5b69f021bf3ba696958dc1
imjoseangel <josea.munoz@gmail.com>
2022-03-05 00:14:11 +0100

    add shorter

--- python/postk8s/main.py
+++ python/postk8s/main.py
@@ -4,15 +4,12 @@
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
-from dataclasses import dataclass, field
-import base64
-import json
+from dataclasses import dataclass
 import logging
-import os
 import sys
 from waitress import serve
 import requests
-from flask import Flask
+from flask import Flask, request
 
 
 @dataclass
@@ -25,30 +22,12 @@ class RunJob:
         stream=sys.stderr,
     )
 
-    auth: str = field(default="")
-    encodeauth: str = field(default="")
-    headers: dict = field(default_factory=dict)
-    secret: str = os.getenv("secret", "")
-
-    def __post_init__(self):
-        self.authencode()
-
-    def authencode(self):
-
-        self.auth = f':{self.secret}'
-        self.encodeauth = base64.b64encode(self.auth.encode('utf-8'))
-
-        self.headers = {
-            'Authorization':
-            f'Basic {self.encodeauth.decode("utf-8")}'
-        }
-
     def get_running(self):
 
         url = ("https://www.google.com")
 
         try:
-            response = requests.request("GET", url, headers=self.headers)
+            response = requests.request("GET", url)
         except NameError as e:
             logging.error(e)
 
@@ -59,10 +38,12 @@ app = Flask(__name__)
 jobrequests = RunJob()
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home():
-    print(jobrequests.get_running(), file=sys.stderr)
-    return jobrequests.get_running()
+    if request.method == 'POST':
+        return jobrequests.get_running()
+    else:
+        return 'GET Method not allowed'
 
 
 def main():
