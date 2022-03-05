1da569512fa5b5115ff338616f22c2fdccbc5d18
imjoseangel <josea.munoz@gmail.com>
2022-03-05 22:56:12 +0100

    add main

---
+++ go/minimalwebserver/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintf(w, "Hello")
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+}
