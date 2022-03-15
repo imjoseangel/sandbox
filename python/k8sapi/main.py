5c4651902a5602cd2c6fd1e8e6d67840ae0d0488
imjoseangel <josea.munoz@gmail.com>
2022-03-15 13:22:31 +0100

    add test

--- python/k8sapi/main.py
+++ python/k8sapi/main.py
@@ -16,6 +16,30 @@ from waitress import serve
 from flask import Flask, request
 from sklearn import tree
 
+from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry import trace
+
+from opentelemetry.sdk.resources import SERVICE_NAME, SERVICE_NAMESPACE, SERVICE_INSTANCE_ID, Resource
+trace.set_tracer_provider(
+    TracerProvider(
+        resource=Resource.create(
+            {
+                SERVICE_NAME: "Kubernetes Test",
+                # ----------------------------------------
+                # Setting role name and role instance
+                # ----------------------------------------
+                SERVICE_NAMESPACE: "kubernetes",
+                SERVICE_INSTANCE_ID: "k8stest",
+                # ----------------------------------------------
+                # Done setting role name and role instance
+                # ----------------------------------------------
+            }
+        )
+    )
+)
+
 logging.basicConfig(
     format="%(asctime)s %(levelname)s: %(message)s",
     level=logging.INFO,
@@ -26,12 +50,14 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def animation():
-    anime = "|/-\\"
-    for i in range(10):
-        time.sleep(0.1)
-        sys.stdout.write("\r" + anime[i % len(anime)])
-        sys.stdout.flush()
+exporter = AzureMonitorTraceExporter.from_connection_string(
+    "InstrumentationKey=<your instrumentation key>"
+)
+
+trace.set_tracer_provider(TracerProvider())
+tracer = trace.get_tracer(__name__)
+span_processor = BatchSpanProcessor(exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
 
 
 @ dataclass
@@ -50,46 +76,48 @@ class RunJob:
 
         try:
 
-            syntax = ("Syntax:\n"
-                      "-------\n\n"
-                      "BODY POST:\n\n"
-                      '{\n'
-                      '"size": "<size>",\n'
-                      '"roughness": "<roughness>"\n'
-                      '}')
+            with tracer.start_as_current_span("run"):
 
-            # logging.info(request.environ)
-            # features = [[155, "rough"], [180, "rough"], [135, "smooth"],
-            # [110, "smooth"], etc]  # Input to classifier
-            features = [[155, 0], [180, 0], [135, 1], [110, 1], [300, 0], [320, 0],
-                        [350, 1], [380, 1]]  # scikit-learn requires real-valued features
+                syntax = ("Syntax:\n"
+                          "-------\n\n"
+                          "BODY POST:\n\n"
+                          '{\n'
+                          '"size": "<size>",\n'
+                          '"roughness": "<roughness>"\n'
+                          '}')
 
-            # labels = ["orange", "orange", "apple", "apple", "melon", "melon",
-            # "watermelon", "watermelon"]  # output values
-            labels = ['🍊', '🍊', '🍎', '🍎', '🍈', '🍈', '🍉', '🍉']
+                # logging.info(request.environ)
+                # features = [[155, "rough"], [180, "rough"], [135, "smooth"],
+                # [110, "smooth"], etc]  # Input to classifier
+                features = [[155, 0], [180, 0], [135, 1], [110, 1], [300, 0], [320, 0],
+                            [350, 1], [380, 1]]  # scikit-learn requires real-valued features
 
-            # Training classifier
-            classifier = tree.DecisionTreeClassifier()  # using decision tree classifier
-            classifier = classifier.fit(
-                features, labels)  # Find patterns in data
+                # labels = ["orange", "orange", "apple", "apple", "melon", "melon",
+                # "watermelon", "watermelon"]  # output values
+                labels = ['🍊', '🍊', '🍎', '🍎', '🍈', '🍈', '🍉', '🍉']
 
-            # Making predictions
-            logging.info(request.json)
+                # Training classifier
+                classifier = tree.DecisionTreeClassifier()  # using decision tree classifier
+                classifier = classifier.fit(
+                    features, labels)  # Find patterns in data
 
-            result = classifier.predict(
-                [[request.json['size'], request.json['roughness']]])
+                # Making predictions
+                logger.info(request.json)
 
-            # Output is apple for [120, 1]
-            logging.info({result[0]})
-            return f"{{'{result[0]}'}}"
+                result = classifier.predict(
+                    [[request.json['size'], request.json['roughness']]])
+
+                # Output is apple for [120, 1]
+                logger.info({result[0]})
+                return f"{{'{result[0]}'}}"
 
         except KeyError as e:
-            logging.error(e)
+            logger.error(e)
             return (f"Error: Incorrect {e} \n\n"
                     f"{syntax}")
 
         except TypeError as e:
-            logging.error(e)
+            logger.error(e)
             return (f"Error: Incorrect {e} \n\n"
                     f"{syntax}")
 
@@ -101,10 +129,11 @@ jobrequests = RunJob()
 @ app.route('/', methods=['POST', 'GET'])
 def home():
     if request.method == 'POST':
-        return jobrequests.get_running()
+        with tracer.start_as_current_span("home"):
+            logger.info("Running ML model")
+            return jobrequests.get_running()
     else:
-        logging.info('GET Method not allowed')
-        return 'GET Method not allowed'
+        return 'healthy'
 
 
 def main():
