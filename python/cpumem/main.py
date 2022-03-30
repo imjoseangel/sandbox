1f41e3fa9ad818d5acd06b2cfc2c22a778edf804
imjoseangel <josea.munoz@gmail.com>
2022-03-30 22:39:20 +0200

    add percentages

--- python/cpumem/main.py
+++ python/cpumem/main.py
@@ -7,12 +7,26 @@ Python Template
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 import psutil
+import logging
 
 
 def main():
     """
     Main function
     """
+
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s: %(message)s",
+        level=logging.INFO,
+        datefmt="%d-%b-%y %H:%M:%S",
+    )
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    logger.info("CPU usage: %s", psutil.cpu_percent())
+    logger.info("Memory usage: %s", psutil.virtual_memory().percent)
+    logger.info("Disk usage: %s", psutil.disk_usage('/').percent)
 
 
 if __name__ == '__main__':
