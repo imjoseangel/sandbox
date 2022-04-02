cdb20f07d85bdb1f922d6a8181b741dc6cde464e
imjoseangel <josea.munoz@gmail.com>
2022-04-02 08:45:23 +0200

    add dag

--- python/airflow/simple_bash_dag.py
+++ python/airflow/simple_bash_dag.py
@@ -1,34 +1,35 @@
-#!/usr/bin/python3
-import os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Import all important packages
 from datetime import datetime, timedelta
+from airflow import models
+# imported BashOperator method
+from airflow.operators.bash_operator import BashOperator
+from airflow import DAG
+from airflow.utils.dates import days_ago
 
-from airflow.models import DAG
-from airflow.operators.bash import BashOperator
+# Get the yesterday timestamp
+yesterday = datetime.combine(
+    datetime.today() - timedelta(1),
+    datetime.min.time())
 
-# let's setup arguments for our dag
-
-DAG_ID = os.path.basename(__file__).replace(".py", "")
-
-DEFAULT_ARGS = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'retries': 10,
-    'concurrency': 1
+# Create a dictionary of default arguments for each task to set the task"s constructor
+default_dag_args = {
+    'start_date': yesterday,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'project_id': models.Variable.get('gcp_project')
 }
 
-# dag declaration
-
-dag = DAG(
-    dag_id=DAG_ID,
-    default_args=DEFAULT_ARGS,
-    description="Run a dynamic Bash command",
-    start_date=datetime.now() + timedelta(days=-1),
-    schedule_interval=timedelta(seconds=5)
-)
-
-
-# Here's a task based on Bash Operator!
-
-bash_task = BashOperator(task_id='bash_task_1',
-                         bash_command="env",
-                         dag=dag)
+with DAG(
+        dag_id="any_bash_command_dag",
+        schedule_interval=None,
+        catchup=False,
+        start_date=days_ago(1)) as dag:
+    cli_command = BashOperator(
+        task_id="bash_command",
+        bash_command="{{ dag_run.conf['command'] }}"
+    )
