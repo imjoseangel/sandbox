5f8e243fb059931d8c4e56a8945dd1343865958c
imjoseangel <josea.munoz@gmail.com>
2022-04-09 12:27:28 +0200

    add ijso

--- python/largejson/main.py
+++ python/largejson/main.py
@@ -1,4 +1,5 @@
 import json
+import ijson
 
 with open("large-file.json", "r", encoding="UTF-8") as f:
     data = json.load(f)
@@ -12,3 +13,16 @@ for record in data:
     user_to_repos[user].add(repo)
 
 print(user_to_repos)
+
+
+iuser_to_repos = {}
+
+with open("large-file.json", "r", encoding="UTF-8") as f:
+    for record in ijson.items(f, "item"):
+        user = record["actor"]["login"]
+        repo = record["repo"]["name"]
+        if user not in iuser_to_repos:
+            iuser_to_repos[user] = set()
+        iuser_to_repos[user].add(repo)
+
+print(iuser_to_repos)
