6285af7c6024b6fd71d62f413c3956a3f3bd0196
imjoseangel <josea.munoz@gmail.com>
2022-04-16 11:04:02 +0200

    add main

---
+++ python/birthdayparadox/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Birthday Paradox Simulation, by Al Sweigart al@inventwithpython.com
+Explore the surprising probabilities of the "Birthday Paradox".
+More info at https://en.wikipedia.org/wiki/Birthday_problem
+This code is available at https://nostarch.com/big-book-small-python-programming
+Tags: short, math, simulation"""
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+import datetime
+import random
+
+
+def getBirthdays(numberOfBirthdays):
+    """Returns a list of number random date objects for birthdays."""
+    pass
+
+
+def main():
+    """
+    Main function
+    """
+
+
+if __name__ == '__main__':
+    main()
