9de4f4a90957f858adec1e501a276ae3dc63b977
imjoseangel <josea.munoz@gmail.com>
2022-05-11 23:18:35 +0200

    add iterator

--- python/dunders/main.py
+++ python/dunders/main.py
@@ -9,3 +9,9 @@ class Point:
 
 p = Point(10, 5)
 print(p)
+
+points_list = [Point(i, i * 2) for i in range(1, 5)]
+print(points_list)
+
+points_dict = {str(i): Point(i, i * 3) for i in range(1, 5)}
+print(points_dict)
