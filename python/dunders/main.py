e9b103e77b9691d1c6c881a0d5cbdd93b1d4b377
imjoseangel <josea.munoz@gmail.com>
2022-05-11 23:33:22 +0200

    add len

--- python/dunders/main.py
+++ python/dunders/main.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -6,10 +9,18 @@ class Point:
     def __repr__(self):
         return f"Point(x={self.x}, y={self.y})"
 
-    def __sub__(self, other: Point):
+    def __sub__(self, other):
         x = self.x - other.x
         y = self.y - other.y
         return Point(x, y)
+
+    def magnitude(self):
+        """gives a more precise distance from origin"""
+        return math.sqrt((self.x * self.x) + (self.y * self.y))
+
+    def __len__(self):
+        """returns the approximate distance from origin"""
+        return round(self.magnitude())
 
 
 p = Point(10, 5)
@@ -20,3 +31,7 @@ print(points_list)
 
 points_dict = {str(i): Point(i, i * 3) for i in range(1, 5)}
 print(points_dict)
+
+p = Point(0, 0)
+q = Point(1, 2)
+print(q - p)
