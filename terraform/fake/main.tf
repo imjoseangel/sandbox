275765fd80193a04302fafa87f204bd17b7e4aa9
imjoseangel <josea.munoz@gmail.com>
2022-05-21 23:40:34 +0200

    add  output and null

--- terraform/fake/main.tf
+++ terraform/fake/main.tf
@@ -20,3 +20,20 @@ resource "fakewebservices_database" "prod_db" {
   name = "Production DB"
   size = 256
 }
+
+output "servers" {
+  value = fakewebservices_server.servers
+}
+
+resource "null_resource" "servers" {
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers = {
+    cluster_instance_ids = "${join(",", fakewebservices_server.servers.*.id)}"
+  }
+
+  # Bootstrap script can run on any instance of the cluster
+  # So we just choose the first in this case
+  connection {
+    host = element(fakewebservices_server.servers.*.id, 0)
+  }
+}
