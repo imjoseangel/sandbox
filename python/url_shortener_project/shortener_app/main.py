37e8736cd81f2979e3ef5b11a487469c7a8fec55
imjoseangel <josea.munoz@gmail.com>
2022-05-26 21:13:04 +0200

    add main

--- python/url_shortener_project/shortener_app/main.py
+++ python/url_shortener_project/shortener_app/main.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
+import validators
+from fastapi import FastAPI, HTTPException
+
+from . import schemas
+
 
 app = FastAPI()
+
+
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
 
 
 @app.get("/")
 def read_root():
     return "Welcome to the URL shortener API :)"
+
+
+@app.post("/url")
+def create_url(url: schemas.URLBase):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="Your provided URL is not valid")
+    return f"TODO: Create database entry for: {url.target_url}"
