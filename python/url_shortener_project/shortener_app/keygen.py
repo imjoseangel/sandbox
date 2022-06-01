04f374c71584a3fdfcb2eabb404aa353d77b0c8c
imjoseangel <josea.munoz@gmail.com>
2022-06-01 21:38:34 +0200

    add keygen

---
+++ python/url_shortener_project/shortener_app/keygen.py
@@ -0,0 +1,7 @@
+import secrets
+import string
+
+
+def create_random_key(length: int = 5) -> str:
+    chars = string.ascii_uppercase + string.digits
+    return "".join(secrets.choice(chars) for _ in range(length))
