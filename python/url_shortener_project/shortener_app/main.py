fe89b6c9e2eb1c0d8c21ec651ee9950a54ddbdc9
imjoseangel <josea.munoz@gmail.com>
2022-06-01 20:16:00 +0200

    add redirect

--- python/url_shortener_project/shortener_app/main.py
+++ python/url_shortener_project/shortener_app/main.py
@@ -1,7 +1,8 @@
 import secrets
 
 import validators
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
 from . import models, schemas
@@ -24,9 +25,25 @@ def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
 
 
-@app.get("/")
-def read_root():
-    return "Welcome to the URL shortener API :)"
+def raise_not_found(request):
+    message = f"URL '{request.url}' doesn't exist"
+    raise HTTPException(status_code=404, detail=message)
+
+
+@app.get("/{url_key}")
+def forward_to_target_url(
+        url_key: str,
+        request: Request,
+        db: Session = Depends(get_db)):
+    db_url = (
+        db.query(models.URL)
+        .filter(models.URL.key == url_key, models.URL.is_active)
+        .first()
+    )
+    if db_url:
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
 
 
 @app.post("/url", response_model=schemas.URLInfo)
