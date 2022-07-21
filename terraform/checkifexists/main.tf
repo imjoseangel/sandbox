aac1a788fb4be7e4cb03f02877e8cd2e94c88437
imjoseangel <josea.munoz@gmail.com>
2022-07-21 20:55:11 +0200

    remove experiment

--- terraform/checkifexists/main.tf
+++
@@ -1,8 +0,0 @@
-data "aws_s3_bucket" "exists" {
-  bucket = "zih-zugl-airflow-poc-ec-s3s"
-}
-
-resource "aws_s3_bucket" "main" {
-  count  = data.aws_s3_bucket.exists.bucket == "" ? 1 : 0
-  bucket = "zih-zugl-airflow-poc-ec-s3"
-}
