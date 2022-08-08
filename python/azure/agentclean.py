b04de39f9a9e7b4b53ca1b85227eae35fc31b10b
imjoseangel <josea.munoz@gmail.com>
2022-08-08 14:32:49 +0200

    add agentclean

--- python/azure/agentclean.py
+++ python/azure/agentclean.py
@@ -5,5 +5,20 @@ from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
 import os
+import base64
 
 secret = os.getenv("ADV_TOKEN", "")
+organization = os.getenv("ADV_ORG", "inc")
+poolid = os.getenv("ADV_POOL", "1")
+
+auth = f':{secret}'
+encodeauth = base64.b64encode(auth.encode('utf-8'))
+
+headers = {
+    'Authorization':
+    f'Basic {encodeauth.decode("utf-8")}'
+}
+
+url = (
+    f"https://dev.azure.com/{organization}/"
+    f"_apis/distributedtask/pools/{poolid}/jobrequests")
