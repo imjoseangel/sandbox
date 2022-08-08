198580c9370f1033b79d7f3266f0a81afdf58090
imjoseangel <josea.munoz@gmail.com>
2022-08-08 14:27:33 +0200

    initial stuff

---
+++ python/azure/agentclean.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+import os
+
+secret = os.getenv("ADV_TOKEN", "")
