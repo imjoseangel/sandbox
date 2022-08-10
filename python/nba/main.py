edcbf27e76194391e0fc26d13240a7a16df59414
imjoseangel <josea.munoz@gmail.com>
2022-08-10 19:43:36 +0200

    add first stage

---
+++ python/nba/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+NBA Scraper
+"""
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+import cv2
+import json
+from moviepy.editor import *
+import pandas as pd
+import pytesseract
+import requests
+
+
+def main():
+    """
+    Main function
+    """
+
+    date = '2017-11-01'
+
+    jsn = f"https://data.nba.net/10s/prod/v1/{date}/scoreboard.json"
+    page = requests.get(jsn)
+    j = json.loads(page.content)
+
+    print(j)
+
+
+if __name__ == '__main__':
+    main()
