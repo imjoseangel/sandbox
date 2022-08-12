3f5c68b7cbe597ebfcaf7634f2e7e869c13f55ca
imjoseangel <josea.munoz@gmail.com>
2022-08-12 08:17:34 +0200

    remove class

--- python/azdatalake/nbaapi.py
+++ python/azdatalake/nbaapi.py
@@ -7,14 +7,10 @@ Python Template
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
-from dataclasses import dataclass
-import asyncio
 import logging
 import datetime
 import json
 import os
-import sys
-import time
 import requests
 import pandas as pd
 from waitress import serve
@@ -41,150 +37,142 @@ containerName = "sports"
 directoryName = "nba"
 
 
-@ dataclass
-class RunJob:
+def initialize_storage_account_ad(storage_account_name, client_id,
+                                  client_secret, tenant_id):
 
-    @staticmethod
-    def initialize_storage_account_ad(storage_account_name, client_id,
-                                      client_secret, tenant_id):
+    try:
+        credential = ClientSecretCredential(
+            tenant_id, client_id, client_secret)
 
-        try:
-            credential = ClientSecretCredential(
-                tenant_id, client_id, client_secret)
+        service_client = DataLakeServiceClient(
+            account_url=f"https://{storage_account_name}.dfs.core.windows.net",
+            credential=credential)
 
-            service_client = DataLakeServiceClient(
-                account_url=f"https://{storage_account_name}.dfs.core.windows.net",
-                credential=credential)
+        logger.info(f"connected to {service_client.account_name}")
 
-            logger.info(f"connected to {service_client.account_name}")
+        return service_client
 
-            return service_client
+    except ResourceNotFoundError as e:
+        logger.info(e)
 
-        except ResourceNotFoundError as e:
-            logger.info(e)
+        return None
 
-            return None
+    except HttpResponseError as e:
+        logger.info(e)
 
-        except HttpResponseError as e:
-            logger.info(e)
+    except ValueError as e:
+        logger.info(e)
 
-            return None
 
-    @staticmethod
-    def create_file_system(service_client):
-        try:
-            file_system_client = service_client.create_file_system(
-                file_system=containerName)
+def create_file_system(service_client):
+    try:
+        file_system_client = service_client.create_file_system(
+            file_system=containerName)
 
-            return file_system_client
+        return file_system_client
 
-        except ResourceExistsError:
-            file_system_client = service_client.get_file_system_client(
-                file_system=containerName)
+    except ResourceExistsError:
+        file_system_client = service_client.get_file_system_client(
+            file_system=containerName)
 
-            return file_system_client
+        return file_system_client
 
-        except HttpResponseError as e:
-            logger.info(e)
+    except HttpResponseError as e:
+        logger.info(e)
 
-            return None
+        return None
 
-    @staticmethod
-    def create_directory(file_system_client):
-        try:
-            file_system_client.create_directory(directoryName)
 
-        except ResourceExistsError as e:
-            logger.info(e)
+def create_directory(file_system_client):
+    try:
+        file_system_client.create_directory(directoryName)
 
-        except HttpResponseError as e:
-            logger.info(e)
+    except ResourceExistsError as e:
+        logger.info(e)
 
-    @staticmethod
-    def upload_file_to_directory_bulk(service_client, filename):
-        try:
+    except HttpResponseError as e:
+        logger.info(e)
 
-            file_system_client = service_client.get_file_system_client(
-                file_system=containerName)
 
-            directory_client = file_system_client.get_directory_client(
-                directoryName)
+def upload_file_to_directory_bulk(service_client, filename):
+    try:
 
-            file_client = directory_client.get_file_client(filename)
+        file_system_client = service_client.get_file_system_client(
+            file_system=containerName)
 
-            local_file = open(filename, "r", encoding="utf-8")
+        directory_client = file_system_client.get_directory_client(
+            directoryName)
 
-            file_contents = local_file.read()
-            file_client.upload_data(file_contents, overwrite=True)
+        file_client = directory_client.get_file_client(filename)
 
-            logger.info(f"uploaded {filename} to {directoryName}")
+        local_file = open(filename, "r", encoding="utf-8")
 
-        except HttpResponseError as e:
-            logger.info(e)
+        file_contents = local_file.read()
+        file_client.upload_data(file_contents, overwrite=True)
 
-    @staticmethod
-    def get_json(service_client):
+        logger.info(f"uploaded {filename} to {directoryName}")
 
-        mainjsn = "https://data.nba.net/data/10s/prod/v1/calendar.json"
+    except HttpResponseError as e:
+        logger.info(e)
 
-        nbanet = requests.get(mainjsn)
-        nbadates = json.loads(nbanet.content)
 
-        for nbadate in nbadates:
+def get_json(service_client):
 
-            try:
-                datetime.datetime.strptime(nbadate, '%Y%m%d')
+    mainjsn = "https://data.nba.net/data/10s/prod/v1/calendar.json"
 
-                scoreboard = f"https://data.nba.net/10s/prod/v1/{nbadate}/scoreboard.json"
-                gameday = requests.get(scoreboard)
-                games = json.loads(gameday.content)
+    nbanet = requests.get(mainjsn)
+    nbadates = json.loads(nbanet.content)
 
-                pdObj = pd.read_json(json.dumps(games['games']))
-                pdObj.to_csv(f"{nbadate}.csv", index=False)
-
-                RunJob.upload_file_to_directory_bulk(
-                    service_client, filename=f"{nbadate}.csv")
-
-                os.remove(f"{nbadate}.csv")
-
-                logger.info(pdObj)
-
-            except ValueError:
-                pass
-
-    @staticmethod
-    def animation():
-        anime = "|/-\\"
-        for i in range(10):
-            time.sleep(0.1)
-            sys.stdout.write("\r" + anime[i % len(anime)])
-            sys.stdout.flush()
-
-    @staticmethod
-    def get_running():
+    for nbadate in nbadates:
 
         try:
+            datetime.datetime.strptime(nbadate, '%Y%m%d')
 
-            client = RunJob.initialize_storage_account_ad(
-                storageAccountName, clientid, clientsecret, tenantid)
-            file_system = RunJob.create_file_system(client)
-            RunJob.create_directory(file_system)
+            scoreboard = f"https://data.nba.net/10s/prod/v1/{nbadate}/scoreboard.json"
+            gameday = requests.get(scoreboard)
+            games = json.loads(gameday.content)
 
-            RunJob.get_json(client)
+            pdObj = pd.read_json(json.dumps(games['games']))
+            pdObj.to_csv(f"{nbadate}.csv", index=False)
 
-            return "Done"
+            upload_file_to_directory_bulk(
+                service_client, filename=f"{nbadate}.csv")
 
-        except KeyError as e:
-            logger.error(e)
-            return f"Error: Incorrect {e}"
+            os.remove(f"{nbadate}.csv")
 
-        except TypeError as e:
-            logger.error(e)
-            return f"Error: Incorrect {e}"
+            logger.info(pdObj)
+
+        except ValueError:
+            pass
+
+
+def get_running():
+
+    try:
+
+        client = initialize_storage_account_ad(
+            storageAccountName, clientid, clientsecret, tenantid)
+        file_system = create_file_system(client)
+        create_directory(file_system)
+
+        get_json(client)
+
+        return "Done"
+
+    except KeyError as e:
+        logger.error(e)
+        return f"Error: Incorrect {e}"
+
+    except TypeError as e:
+        logger.error(e)
+        return f"Error: Incorrect {e}"
+
+    except Exception as e:
+        logger.error(e)
+        return f"Error: Incorrect {e}"
 
 
 app = Flask(__name__)
-jobrequests = RunJob()
 
 
 @app.route('/', methods=['GET'])
@@ -192,7 +180,7 @@ def home():
     if request.method == 'GET':
 
         logger.info("Running NBA")
-        jobrequests.get_running()
+        get_running()
         return "Running NBA"
 
     else:
