e662f1c933dd1ead61f64cf2b3962b7f9109ec9c
imjoseangel <josea.munoz@gmail.com>
2022-08-13 18:37:22 +0200

    add iteration

--- python/nba/iterate.py
+++ python/nba/iterate.py
@@ -7,9 +7,9 @@ NBA Test
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
-import datetime
 import json
 import requests
+import pandas as pd
 
 
 def get_json():
@@ -19,7 +19,13 @@ def get_json():
 
     nbadates = json.loads(nbanet.content)
 
-    output = (nbadate for nbadate in nbadates if nbadate.startswith('20'))
+    gameday = (requests.get(
+        f"https://data.nba.net/10s/prod/v1/{nbadate}/scoreboard.json")
+        for nbadate in (nbadate for nbadate in nbadates
+                        if nbadate.startswith('20')))
+
+    for game in gameday:
+        print(game.content)
 
 
 def main():
