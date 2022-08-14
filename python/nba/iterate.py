479dc4345c9507cf44ff783ea41642bf313b0168
imjoseangel <josea.munoz@gmail.com>
2022-08-14 09:34:35 +0200

    add games

--- python/nba/iterate.py
+++ python/nba/iterate.py
@@ -10,8 +10,13 @@ from __future__ import (division, absolute_import, print_function,
 import json
 import requests
 import pandas as pd
+from celery import Celery
+
+app = Celery('tasks', backend='redis://localhost', broker='pyamqp://')
+app.conf.broker_url = 'redis://localhost:6379/0'
 
 
+@app.task
 def get_json():
 
     mainjsn = "https://data.nba.net/data/10s/prod/v1/calendar.json"
@@ -19,11 +24,13 @@ def get_json():
 
     nbadates = json.loads(nbanet.content)
 
-    pdObj = (pd.read_json(json.dumps(game['games'])) for game in
+    pdObj = (pd.read_json(json.dumps(games['games'])) for games in
              (json.loads(gameday.content) for gameday in (requests.get(
                  f"https://data.nba.net/10s/prod/v1/{nbadate}/scoreboard.json")
                  for nbadate in (nbadate for nbadate in nbadates
                                  if nbadate.startswith('20')))))
+
+    print(list(pdObj))
 
 
 def main():
