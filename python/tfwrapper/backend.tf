f5ad39ae4229712480002d28b45c8775a0bfa694
imjoseangel <josea.munoz@gmail.com>
2022-08-17 13:39:18 +0200

    add backend

--- python/tfwrapper/backend.tf
+++ python/tfwrapper/backend.tf
@@ -1,7 +1,3 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "rsg-sharedresources"
-    storage_account_name = "storageterraform"
-    container_name       = "tfstate"
-  }
+  backend "local" {}
 }
