ba75a1740c4e5147b00adf362eb8143c99b40411
imjoseangel <josea.munoz@gmail.com>
2022-09-07 23:13:50 +0200

    add cors

--- python/trivia/trivia_api/trivia_api/settings.py
+++ python/trivia/trivia_api/trivia_api/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -124,3 +125,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+]
