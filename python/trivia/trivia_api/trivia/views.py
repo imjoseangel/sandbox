46ba8cb4702631dfff69c8ce73a24a259f0bb874
imjoseangel <josea.munoz@gmail.com>
2022-09-07 23:09:15 +0200

    add serializer

--- python/trivia/trivia_api/trivia/views.py
+++ python/trivia/trivia_api/trivia/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+# from django.shortcuts import render
 # Create your views here.
+
+from rest_framework import viewsets
+
+from .models import Question
+from .serializers import QuestionSerializer
+
+
+class QuestionViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
