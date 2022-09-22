799d5621a6f3559216c58b7105c37108f4892baa
imjoseangel <josea.munoz@gmail.com>
2022-09-22 21:26:57 +0200

    add internal objects example

---
+++ python/subclass/counter.py
@@ -0,0 +1,23 @@
+class ObjCounter:
+    # set the private 'counter' here
+    __counter = 0
+
+    def __init__(self, val=1):
+        # set a private property 'first' to val
+        self.__first = val
+        # add here the counter
+        ObjCounter.__counter += 1
+
+
+object_1 = ObjCounter()
+object_2 = ObjCounter(2)
+object_3 = ObjCounter(4)
+
+print("Object_2 first: ", object_2._ObjCounter__first)
+print("Object_3 first: ", object_3._ObjCounter__first)
+
+print("We have created", end=" ")
+print(object_3._ObjCounter__counter, "objects")
+
+print("Counter inside the ObjCounter is now", end=" ")
+print(ObjCounter.__dict__["_ObjCounter__counter"])
