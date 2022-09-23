2e2ae40d535db34bb2ec70cf2e5992b7a609101d
imjoseangel <josea.munoz@gmail.com>
2022-09-23 22:58:52 +0200

    add main

---
+++ python/inheritance/main.py
@@ -0,0 +1,42 @@
+class Document:
+    def __init__(self, name):
+        self.name = name
+
+    def path(self, path):
+        return path
+
+    def save(self, path, extention):
+        return self.path(path) + self.name + extention
+
+    def __str__(self):
+        return "This is a Document"
+
+
+class PDF(Document):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def save(self, path):
+        return self.path(path) + self.name + ".pdf"
+
+    def __str__(self):
+        return "This is a PDF Document"
+
+
+class TEXT(Document):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def save(self, path):
+        return self.path(path) + self.name + ".txt"
+
+    def __str__(self):
+        return "This is a Text Document"
+
+
+pdf = PDF(name="slides-show")
+txt = TEXT(name="slides-content")
+
+print(txt, txt.save("/home/txt/"))
+print(pdf, pdf.save("/home/pdf/"))
+print(txt, txt.save("/home/txt/"))
