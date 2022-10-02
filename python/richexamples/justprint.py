8de7d173c1478a855bce3802dc7a00099513833d
imjoseangel <josea.munoz@gmail.com>
2022-10-02 10:13:19 +0200

    add print

---
+++ python/richexamples/justprint.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from rich import print
+
+nums_list = [1, 2, 3, 4]
+print(nums_list)
+
+nums_tuple = (1, 2, 3, 4)
+print(nums_tuple)
+
+nums_dict = dict(nums_list=nums_list, nums_tupple=nums_tuple)
+print(nums_dict)
+
+bool_list = [True, False]
+print(bool_list)
