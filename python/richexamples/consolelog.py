106a59af39fe5c4ea9d883b8e4ab4eeb3e32ab58
imjoseangel <josea.munoz@gmail.com>
2022-10-02 11:25:33 +0200

    add log

---
+++ python/richexamples/consolelog.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from rich.console import Console
+
+console = Console()
+
+
+def merge_dict(dict_one, dict_two):
+    merged_dict = dict_one | dict_two
+    console.log(merged_dict, log_locals=True)
+
+
+merge_dict({'id': 1}, {'name': 'Ashutosh'})
