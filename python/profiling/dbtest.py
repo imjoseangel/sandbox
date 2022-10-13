7c7fe01924f8dac92fa3526a0558f4cea5b551bf
imjoseangel <josea.munoz@gmail.com>
2022-10-13 09:16:02 +0200

    add mprofile

--- python/profiling/dbtest.py
+++ python/profiling/dbtest.py
@@ -1,4 +1,3 @@
-import os
 from databricks import sql
 from memory_profiler import profile
 
@@ -6,9 +5,9 @@ from memory_profiler import profile
 @profile
 def sqlconnect():
     try:
-        with sql.connect(server_hostname=os.getenv("DATABRICKS_SERVER_HOSTNAME"),
-                         http_path=os.getenv("DATABRICKS_HTTP_PATH"),
-                         access_token=os.getenv("DATABRICKS_TOKEN")):
+        with sql.connect(server_hostname="xx.azuredatabricks.net",
+                         http_path="/sql/1.0/endpoints/xx",
+                         access_token="DATABRICKS_TOKEN") as connection:
 
             ...
 
