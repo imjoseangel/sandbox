2fa095b1665b8b0e0149e07742284d9c9adeae03
imjoseangel <josea.munoz@gmail.com>
2022-10-15 23:28:51 +0200

    add another commit

--- python/azdevopswithgit/main.py
+++ python/azdevopswithgit/main.py
@@ -5,12 +5,21 @@ import os
 import git
 
 
-def get_git_root() -> str:
+def get_git_root() -> git.Repo:
 
     repo = git.Repo(os.path.dirname(os.path.realpath(__file__)),
                     search_parent_directories=True)
     git_root = repo.git.rev_parse("--show-toplevel")
-    return git_root
+    return repo
 
 
-print(get_git_root())
+def diff_commit(repo):
+    headcommit = repo.head.commit
+    diff = headcommit.diff("HEAD^")
+    return diff
+
+
+repo = get_git_root()
+
+for x in diff_commit(repo):
+    print(x)
