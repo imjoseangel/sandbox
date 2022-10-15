798d29ba66440c5d42e05a62c6d3f7f9da28dafd
imjoseangel <josea.munoz@gmail.com>
2022-10-15 23:23:11 +0200

    get root

--- python/azdevopswithgit/main.py
+++ python/azdevopswithgit/main.py
@@ -5,11 +5,6 @@ import os
 import git
 
 
-def find_commit_by_tag(tag):
-    repo = git.Repo('.')
-    return repo.commit(tag)
-
-
 def get_git_root() -> str:
 
     repo = git.Repo(os.path.dirname(os.path.realpath(__file__)),
