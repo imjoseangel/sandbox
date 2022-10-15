a7db3acfb504882f80211c820be369a23006dd32
imjoseangel <josea.munoz@gmail.com>
2022-10-15 23:42:35 +0200

    git diff

--- python/azdevopswithgit/main.py
+++ python/azdevopswithgit/main.py
@@ -9,16 +9,16 @@ def get_git_root() -> Repo:
 
     gitrepo = Repo(os.path.dirname(os.path.realpath(__file__)),
                    search_parent_directories=True)
-    gitroot = gitrepo.git.rev_parse("--show-toplevel")
     return gitrepo
 
 
+def get_git_name() -> str:
+    return get_git_root().git.rev_parse("--show-toplevel")
+
+
 def diff_commit(gitrepo):
-    headcommit = gitrepo.head.commit
-    diff = headcommit.diff("HEAD~1", name_only=True)
+    diff = gitrepo.git.diff("HEAD^", name_only=True)
     return diff
 
 
-repo = get_git_root()
-
-print(diff_commit(repo))
+print(diff_commit(get_git_root()))
