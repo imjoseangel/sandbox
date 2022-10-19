927473e382956fd24b22c156a02d3becb8cc94ab
imjoseangel <josea.munoz@gmail.com>
2022-10-19 22:45:53 +0200

    add rsa

--- python/jwts-in-python/main.py
+++ python/jwts-in-python/main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import jwt
-
+from cryptography.hazmat.primitives import serialization
 
 payload_data = {
     "sub": "4242",
@@ -16,3 +16,17 @@ token = jwt.encode(
     payload=payload_data,
     key=my_secret
 )
+
+print(token)
+
+# read and load the key
+private_key = open('/Users/imjoseangel/.ssh/id_rsa',
+                   'r', encoding='UTF-8').read()
+key = serialization.load_ssh_private_key(private_key.encode(), password=b'')
+
+token_rsa = jwt.encode(
+    payload=payload_data,
+    key=my_secret
+)
+
+print(token_rsa)
