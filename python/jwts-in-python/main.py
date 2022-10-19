7a48f40d149ea813f7427fa73539aa8f9fb3f3cc
imjoseangel <josea.munoz@gmail.com>
2022-10-19 23:06:21 +0200

    add jwt

--- python/jwts-in-python/main.py
+++ python/jwts-in-python/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import jwt
+from jwt.exceptions import ExpiredSignatureError, InvalidAlgorithmError
 from cryptography.hazmat.primitives import serialization
 
 payload_data = {
@@ -43,3 +43,20 @@ print(jwt.decode(
     key='my_super_secret',
     algorithms=[header_data['alg'], ]
 ))
+
+public_key = open('/Users/imjoseangel/.ssh/id_rsa.pub',
+                  'r', encoding='UTF-8').read()
+key = serialization.load_ssh_public_key(public_key.encode())
+
+print(jwt.decode(jwt=new_token, key=key, algorithms=['RS256', ]))
+
+header_data = jwt.get_unverified_header(new_token)
+
+try:
+    payload = jwt.decode(
+        token,
+        key='my_super_secret',
+        algorithms=[header_data['alg'], ]
+    )
+except (ExpiredSignatureError, InvalidAlgorithmError) as error:
+    print(f'Unable to decode the token, error: {error}')
