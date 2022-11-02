bb17c4cb6b4af72cd89440b1c848451a8235ecf3
imjoseangel <josea.munoz@gmail.com>
2022-11-02 16:42:02 +0100

    add memo

--- go/memoization/main.go
+++ go/memoization/main.go
@@ -6,13 +6,13 @@ import (
 )
 
 var memoF = Memo(func(a int) string {
-	// expensive computation:
-	time.Sleep(time.Millisecond * time.Duration(a))
-	return fmt.Sprint(a)
+    // expensive computation:
+    time.Sleep(time.Millisecond * time.Duration(a))
+    return fmt.Sprint(a)
 })
 
 r := memoF(2) // the first call is slow
-r = memoF(2) // other calls are fast
+r = memoF(2)  // other calls are fast
 
 fmt.Println(r)
 // Output: 2
