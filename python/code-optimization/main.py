42a2beb0fa392fc422daad67ea3d530f373dc983
imjoseangel <josea.munoz@gmail.com>
2022-11-07 21:55:56 +0100

    add code

---
+++ python/code-optimization/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def standardize_name(street_name):
+    standard_street_names = [
+        "Brattle St",
+        "Mount Auburn St",
+        "Massachusetts Ave",
+        "Cardinal Medeiros Ave",
+        "Hampshire Street",
+        "Beacon St",
+        "Blake St",
+        "Beech St",
+        "Garden St",
+    ]
+
+    # Exact match:
+    if street_name in standard_street_names:
+        return street_name
+
+    # Different case:
+    lower_name = street_name.lower()
+    for street in standard_street_names:
+        if lower_name == street.lower():
+            return street
+
+    # "Ave." and "Avenue" are possible synonyms of "Ave":
+    parts = street_name.split()
+    if parts[-1].lower() in ("ave.", "avenue"):
+        parts[-1] = "Ave"
+        fixed_street_name = " ".join(parts)
+        return standardize_name(fixed_street_name)
