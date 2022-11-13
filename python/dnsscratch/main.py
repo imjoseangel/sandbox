1e9c751358593873ad4fe757d8d17c24326f3b52
imjoseangel <josea.munoz@gmail.com>
2022-11-13 11:41:02 +0100

    dns python

--- python/dnsscratch/main.py
+++ python/dnsscratch/main.py
@@ -2,10 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import socket
+from rich import print as rprint
+
+HEX = "c9980120000100000000000103777777076578616d706c6503636f6d00000100010000291000000000000000"
 
 sock = socket.socket()
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 x = sock.bind(('0.0.0.0', 12345))
 y = sock.connect(('8.8.8.8', 53))
 
-print(x, y)
+bytestream = bytes.fromhex(HEX)
+rprint(f'Sent: {bytestream}')
+
+sock.send(bytestream)
+
+# get the reply
+reply, _ = sock.recvfrom(1024)
+rprint(f'Recv: {reply}')
