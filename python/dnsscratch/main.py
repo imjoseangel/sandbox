877750b62c45c5bf538ca064d07efd25c84f0407
imjoseangel <josea.munoz@gmail.com>
2022-11-13 14:16:21 +0100

    almost there

--- python/dnsscratch/main.py
+++ python/dnsscratch/main.py
@@ -6,17 +6,47 @@ from rich import print as rprint
 
 HEX = "c9980120000100000000000103777777076578616d706c6503636f6d00000100010000291000000000000000"
 
-sock = socket.socket()
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-x = sock.bind(('0.0.0.0', 12345))
-y = sock.connect(('8.8.8.8', 53))
+def make_question_header(query_id):
+    """
+    :param query_id:
+    :return header:
+    """
+    # id, flags, num questions, num answers, num auth, num additional
+    header = [query_id, 0x0120, 0x0001, 0x0000, 0x0000, 0x0001]
+    return ''.join([f'{field:04x}' for field in header])
 
-bytestream = bytes.fromhex(HEX)
-rprint(f'Sent: {bytestream}')
 
-sock.send(bytestream)
+def encode_domain_name(domain):
+    """
+    :param domain:
+    :return:
+    """
+    parts = domain.split('.')
+    parts = list(map(lambda s: chr(len(s)) + s, parts))
 
-# get the reply
-reply, _ = sock.recvfrom(1024)
-rprint(f'Recv: {reply}')
+    return ''.join(parts).encode()
+
+
+def make_dns_query(domain, type):
+    pass
+
+
+print(make_question_header(0xc998))
+print(encode_domain_name('example.com'))
+
+
+# sock = socket.socket()
+# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# x = sock.bind(('0.0.0.0', 12345))
+# y = sock.connect(('8.8.8.8', 53))
+
+# bytestream = bytes.fromhex(HEX)
+# rprint(f'Sent: {bytestream}')
+
+# sock.send(bytestream)
+
+# # get the reply
+# reply, _ = sock.recvfrom(1024)
+# rprint(f'Recv: {reply}')
