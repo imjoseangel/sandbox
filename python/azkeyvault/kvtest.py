640956e61cccf68a1e90479933e1c334e770cd52
imjoseangel <josea.munoz@gmail.com>
2022-11-28 22:00:16 +0100

    add right credentials

--- python/azkeyvault/kvtest.py
+++ python/azkeyvault/kvtest.py
@@ -6,7 +6,6 @@ from azure.identity import DefaultAzureCredential
 secretName = os.getenv('AZURE_SECRET_NAME')
 keyVaultName = os.getenv('AZURE_KEY_VAULT_NAME')
 
-
 KVUri = f"https://{keyVaultName}.vault.azure.net"
 
 credential = DefaultAzureCredential()
@@ -15,6 +14,11 @@ client = SecretClient(vault_url=KVUri, credential=credential)
 
 print(f"Retrieving your secret from {keyVaultName}.")
 
-retrieved_secret = client.get_secret(secretName)
-print(f"Your secret is '{retrieved_secret.value}'.")
+try:
+    retrieved_secret = client.get_secret(secretName)
+    print(f"Your secret is '{retrieved_secret.value}'.")
+except ValueError:
+    pass
+
+
 print("done.")
