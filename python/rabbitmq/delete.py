2c960c737bc19ef6a799b4b34e38a9d0b68530f5
imjoseangel <josea.munoz@gmail.com>
2022-12-03 22:13:42 +0100

    add delete channel

---
+++ python/rabbitmq/delete.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pika
+
+QUEUE_NAME = "qu.transfer.tst"
+
+credentials = pika.PlainCredentials('user', 'password')
+
+with pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost', credentials=credentials)) as connection:
+    channel = connection.channel()
+    channel.queue_delete(queue=QUEUE_NAME)
