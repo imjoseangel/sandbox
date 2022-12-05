ebaf53d8f4e8a18304733788d4b652c96fafe9a6
Jose Angel Munoz <5019972+imjoseangel@users.noreply.github.com>
2022-12-05 08:55:31 +0100

    Update delete_queue.py

--- python/rabbitmq/delete_queue.py
+++ python/rabbitmq/delete_queue.py
@@ -5,10 +5,10 @@ import pika
 
 QUEUE_NAME = "qu.transfer.tst"
 
-credentials = pika.PlainCredentials("user", "SP94Tea7TnRmHTea")
+credentials = pika.PlainCredentials("user", "password")
 
 with pika.BlockingConnection(pika.ConnectionParameters(
-        host="20.93.216.134", credentials=credentials)) as connection:
+        host="localhost", credentials=credentials)) as connection:
     channel = connection.channel()
     channel.queue_delete(queue=QUEUE_NAME)
     connection.close()
