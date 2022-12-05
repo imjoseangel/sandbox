50b52560799fb2997903d4e7cb0f6a12e4516e61
Jose Angel Munoz <5019972+imjoseangel@users.noreply.github.com>
2022-12-05 08:54:32 +0100

    Update consumer_exchange_fanout.py

--- python/rabbitmq/consumer_exchange_fanout.py
+++ python/rabbitmq/consumer_exchange_fanout.py
@@ -11,10 +11,10 @@ def callback(channel, method, properties, body):
     print(f"Got message: {message}")
 
 
-credentials = pika.PlainCredentials("user", "SP94Tea7TnRmHTea")
+credentials = pika.PlainCredentials("user", "password")
 
 with pika.BlockingConnection(pika.ConnectionParameters(
-        host="20.93.216.134", credentials=credentials)) as connection:
+        host="localhost", credentials=credentials)) as connection:
     try:
         channel = connection.channel()
 
