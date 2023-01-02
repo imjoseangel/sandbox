1a58853e84b79d70ed43e0c0e54ce8b74d3d40c7
imjoseangel <josea.munoz@gmail.com>
2023-01-02 16:51:29 +0100

    add python

--- python/py311/programmers.py
+++ python/py311/programmers.py
@@ -1,5 +1,7 @@
 # programmers.py
 from dataclasses import dataclass
+# This works with python >= 3.11
+from typing import Any, Self
 import json
 import pathlib
 
@@ -13,7 +15,7 @@ class Person:
     life_span: tuple[int, int]
 
     @classmethod
-    def from_dict(cls, info):
+    def from_dict(cls, info: dict[str, Any]) -> Self:
         return cls(
             name=f"{info['name']['first']} {info['name']['last']}",
             life_span=(info["birth"]["year"], info["death"]["year"]),
