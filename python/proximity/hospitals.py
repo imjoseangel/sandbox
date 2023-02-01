73747b2aaa674fb48082f8a072b2b709294ad468
imjoseangel <josea.munoz@gmail.com>
2023-02-01 22:36:29 +0100

    split

--- python/proximity/hospitals.py
+++ python/proximity/hospitals.py
@@ -9,7 +9,8 @@ sd_tracts = tracts("SD", cb=True,
                    year=2021,
                    cache=True).to_crs(6571)
 
-hospital_url = "https://opendata.arcgis.com/api/v3/datasets/6ac5e325468c4cb9b905f1728d6fbf0f_0/downloads/data?format=geojson&spatialRefId=4326"
+hospital_url = "https://opendata.arcgis.com/api/v3/datasets/6ac5e325468c4cb9b905f1728d6fbf0f_0/\
+downloads/data?format=geojson&spatialRefId=4326"
 
 
 hospitals = gp.read_file(hospital_url).to_crs(6571)
