1a7817112d229f6f837732a2e7d1740c54e938cc
imjoseangel <josea.munoz@gmail.com>
2023-02-14 22:33:38 +0100

    add lis

--- python/wordle/wyrdl.py
+++ python/wordle/wyrdl.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import pathlib
+import random
 
-WORD = "SNAKE"
+WORDLIST = pathlib.Path("wordlist.txt")
+
+words = [
+    word.upper()
+    for word in WORDLIST.read_text(encoding="utf-8").strip().split("\n")
+]
+word = random.choice(words)
 
 for guess_num in range(1, 7):
     guess = input(f"\nGuess {guess_num}: ").upper()
 
-    if guess == WORD:
+    if guess == word:
         print("Correct")
         break
 
     correct_letters = {
-        letter for letter, correct in zip(guess, WORD) if letter == correct
+        letter for letter, correct in zip(guess, word) if letter == correct
     }
 
-    misplaced_letters = set(guess) & set(WORD) - correct_letters
-    wrong_letters = set(guess) - set(WORD)
+    misplaced_letters = set(guess) & set(word) - correct_letters
+    wrong_letters = set(guess) - set(word)
 
     print("Correct letters:", ", ".join(sorted(correct_letters)))
     print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
