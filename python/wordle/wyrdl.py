a9db59e7fbf54467391307737f2349d0f94d0138
imjoseangel <josea.munoz@gmail.com>
2023-02-14 22:21:29 +0100

    add comparisson

--- python/wordle/wyrdl.py
+++ python/wordle/wyrdl.py
@@ -10,4 +10,13 @@ for guess_num in range(1, 7):
         print("Correct")
         break
 
-    print("Wrong")
+    correct_letters = {
+        letter for letter, correct in zip(guess, WORD) if letter == correct
+    }
+
+    misplaced_letters = set(guess) & set(WORD) - correct_letters
+    wrong_letters = set(guess) - set(WORD)
+
+    print("Correct letters:", ", ".join(sorted(correct_letters)))
+    print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
+    print("Wrong letters:", ", ".join(sorted(wrong_letters)))
