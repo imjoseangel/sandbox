e80891d096fc66513486c682d098769200a8dc5c
imjoseangel <josea.munoz@gmail.com>
2023-02-14 22:18:13 +0100

    add var

--- python/wordle/wyrdl.py
+++ python/wordle/wyrdl.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+WORD = "SNAKE"
+
 for guess_num in range(1, 7):
     guess = input(f"\nGuess {guess_num}: ").upper()
 
-    if guess == "SNAKE":
+    if guess == WORD:
         print("Correct")
         break
 
