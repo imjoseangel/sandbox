aec9170cd29c1c28d449ee2996724c61a804e245
imjoseangel <josea.munoz@gmail.com>
2023-02-23 10:56:01 +0100

    add exceptions

--- python/wordle/create_wordlist.py
+++ python/wordle/create_wordlist.py
@@ -5,15 +5,24 @@ import pathlib
 import sys
 from string import ascii_letters
 
-in_path = pathlib.Path(sys.argv[1])
-out_path = pathlib.Path(sys.argv[2])
+try:
+    in_path = pathlib.Path(sys.argv[1])
+    out_path = pathlib.Path(sys.argv[2])
+except IndexError as e:
+    print(e)
+    sys.exit(1)
 
-words = sorted(
-    {
-        word.lower()
-        for word in in_path.read_text(encoding="utf-8").split()
-        if all(letter in ascii_letters for letter in word)
-    },
-    key=lambda word: (len(word), word),
-)
-out_path.write_text("\n".join(words), encoding="utf-8")
+try:
+    words = sorted(
+        {
+            word.lower()
+            for word in in_path.read_text(encoding="utf-8").split()
+            if all(letter in ascii_letters for letter in word)
+        },
+        key=lambda word: (len(word), word),
+    )
+    out_path.write_text("\n".join(words), encoding="utf-8")
+
+except FileNotFoundError as e:
+    print(e)
+    sys.exit(1)
