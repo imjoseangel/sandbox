39286e015cb9cc58a04dbfbe6e5e6426330d3cec
imjoseangel <josea.munoz@gmail.com>
2023-02-26 10:34:55 +0100

    add lambdas

--- python/lambdas/main.py
+++ python/lambdas/main.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# This is equel 3
+# This is equal 3
 (lambda x: print(x + 1))(2)
 
+# And this
 add_one = (lambda x: x + 1)
 print(add_one(2))
 
-
+# This prints Full name: Guido Van Rossum
 full_name = (lambda first, last: f'Full name: {first.title()} {last.title()}')
 print(full_name('guido', 'van rossum'))
 
-
+# This is equal 5
 (lambda x, y: print(x + y))(2, 3)
 
 
+# This is equal 6 and 7 respectively
 high_ord_func = (lambda x, func: x + func(x))
 
 print(high_ord_func(2, lambda x: x * x))
