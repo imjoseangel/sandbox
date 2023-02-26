215ea136694f8a5ff3874036179b63e07e0a31ef
imjoseangel <josea.munoz@gmail.com>
2023-02-26 11:00:18 +0100

    add arguments

--- python/lambdas/main.py
+++ python/lambdas/main.py
@@ -47,3 +47,17 @@ print((lambda x:
       (x % 2 and 'odd' or 'even'))(3))
 
 lambda x: x % 2 and 'odd' or 'even'
+
+# Arguments all printing 6
+
+print((lambda x, y, z: x + y + z)(1, 2, 3))
+
+print((lambda x, y, z=3: x + y + z)(1, 2))
+
+print((lambda x, y, z=3: x + y + z)(1, y=2))
+
+print((lambda *args: sum(args))(1, 2, 3))
+
+print((lambda **kwargs: sum(kwargs.values()))(one=1, two=2, three=3))
+
+print((lambda x, *, y=0, z=0: x + y + z)(1, y=2, z=3))
