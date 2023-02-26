8fe9d7274380e98f031adf61b2b73d175ea0ee1b
imjoseangel <josea.munoz@gmail.com>
2023-02-26 11:18:40 +0100

    add closures

--- python/lambdas/main.py
+++ python/lambdas/main.py
@@ -87,3 +87,15 @@ print(trace(lambda x: x ** 2)(3))
 
 
 print(list(map(trace(lambda x: x * 2), range(3))))
+
+
+# Closures
+
+def outer_func(x):
+    y = 4
+    return lambda z: x + y + z
+
+
+for i in range(3):
+    closure = outer_func(i)
+    print(f"closure({i+5}) = {closure(i+5)}")
