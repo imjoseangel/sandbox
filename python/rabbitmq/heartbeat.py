882d7a9a5e5a2539dbf1499be23b4153e72bff90
imjoseangel <josea.munoz@gmail.com>
2023-04-10 17:41:11 +0200

    another test

--- python/rabbitmq/heartbeat.py
+++ python/rabbitmq/heartbeat.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import pika
+from time import sleep
 QUEUE_NAME = "qu.transfer.tst"
 
 credentials = pika.PlainCredentials('user', 'password')
@@ -13,15 +14,35 @@ def main():
                                        heartbeat=600,
                                        blocked_connection_timeout=300)
 
-    conn = pika.BlockingConnection(params)
-    chann = conn.channel()
-    chann.queue_declare(queue=QUEUE_NAME, arguments={
-        'x-message-ttl': 10800000,
-        'x-dead-letter-exchange': 'dlx'
-    }, durable=True)
+    try:
+        connection = pika.BlockingConnection(params)
+        channel = connection.channel()
+        channel.confirm_delivery()
 
-    chann.basic_publish('', QUEUE_NAME, "abc")
-    conn.close()
+        channel.queue_declare(queue=QUEUE_NAME,
+                              arguments={
+                                  'x-message-ttl': 10800000,
+                                  'x-dead-letter-exchange': 'dlx'
+                              },
+                              durable=True)
+
+        for item in range(100000):
+            channel.basic_publish(exchange='',
+                                  routing_key=QUEUE_NAME,
+                                  body="abc",
+                                  properties=pika.BasicProperties(content_type='text/plain',
+                                                                  delivery_mode=pika.DeliveryMode.Transient))
+            print('Message publish was confirmed')
+            sleep(0.5)
+
+    except (KeyboardInterrupt,
+            EOFError,
+            pika.exceptions.StreamLostError,
+            pika.exceptions.IncompatibleProtocolError,
+            pika.exceptions.ConnectionClosedByBroker,
+            pika.exceptions.ConnectionWrongStateError):
+
+        print('Message could not be confirmed')
 
 
 if __name__ == '__main__':
