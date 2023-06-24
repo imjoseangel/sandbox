70ff7de850950e2de0a8a26fd35da4481962b335
imjoseangel <josea.munoz@gmail.com>
2023-06-24 10:04:30 +0200

    add seed

--- python/sha256rc/hmacmain.py
+++ python/sha256rc/hmacmain.py
@@ -2,6 +2,7 @@ import hmac
 import hashlib
 import sys
 
+
 for counter in range(0, 1000000):
     for seed in range(0, 1000000):
         message = f"{seed}{counter}"
@@ -12,8 +13,18 @@ for counter in range(0, 1000000):
             digestmod=hashlib.sha256
         ).hexdigest()
 
-        print(signature[0:6])
-
         if signature[0:6] == "42126c":
-            print(seed)
-            sys.exit(0)
+            print(seed, "", counter)
+
+            for counter in range(0, 1000000):
+                message = f"{seed}{counter}"
+
+                signature = hmac.new(
+                    bytes(str(seed), 'utf-8'),
+                    msg=bytes(message, 'utf-8'),
+                    digestmod=hashlib.sha256
+                ).hexdigest()
+
+                if signature[0:6] == "d358f6":
+                    print("FOUND")
+                    print(seed, "", counter)
