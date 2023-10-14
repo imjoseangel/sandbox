8b807b68666aab5da6ea62c4209df1a87975f765
imjoseangel <josea.munoz@gmail.com>
2023-10-14 11:40:43 +0200

    improve blob

--- python/gitstuff/find-blobs.py
+++ python/gitstuff/find-blobs.py
@@ -4,7 +4,13 @@ import sys
 for line in sys.stdin:
     line = line.strip()
     filename = f".git/objects/{line[0:2]}/{line[2:]}"
-    with open(filename, "rb") as f:
-        contents = zlib.decompress(f.read())
-        if contents.startswith(b"blob"):
-            print(line)
+
+    try:
+
+        with open(filename, "rb") as f:
+            contents = zlib.decompress(f.read())
+            if contents.startswith(b"blob"):
+                print(line)
+
+    except FileNotFoundError:
+        pass
