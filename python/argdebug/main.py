35176ffb43d78cf0e5208245e40ba0aa5235677e
imjoseangel <josea.munoz@gmail.com>
2023-12-31 10:39:44 +0100

    add dynamic verbose

--- python/argdebug/main.py
+++ python/argdebug/main.py
@@ -11,8 +11,17 @@ class ParseArgs():
 
     def __init__(self) -> None:
 
+        self.debug = {0: "INFO",
+                      1: "WARNING",
+                      2: "DEBUG"}
+
         # Parse arguments passed at cli
         self.parse_arguments()
+
+        logging.basicConfig(format="%(asctime)s - %(message)s",
+                            datefmt="%d-%b-%y %H:%M:%S",
+                            stream=sys.stdout,
+                            level=eval(f'logging.{self.debug.get(self.args.verbose)}'))
 
     def parse_arguments(self):
         """ Parse arguments passed at cli """
@@ -27,11 +36,10 @@ class ParseArgs():
 
         parser.add_argument('-v', '--verbose',
                             help='enable verbose (default 0)',
+                            type=int,
+                            choices=range(0, 3),
                             required=False,
                             default=0)
-
-        logging.basicConfig(format="%(asctime)s - %(message)s",
-                            datefmt="%d-%b-%y %H:%M:%S", stream=sys.stdout, level=logging.INFO)
 
         self.args = parser.parse_args()
 
