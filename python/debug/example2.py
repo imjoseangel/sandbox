a07022f42d1ebc213aaf608c079a869a3aed0900
imjoseangel <josea.munoz@gmail.com>
2024-02-03 23:59:10 +0100

    add examples

---
+++ python/debug/example2.py
@@ -0,0 +1,16 @@
+from birdseye import eye
+
+
+@eye
+def number_to_bits(number):
+    if number:
+        bits = []
+        while number:
+            number, remainder = divmod(number, 2)
+            bits.insert(0, remainder)
+        return bits
+    else:
+        return [0]
+
+
+number_to_bits(6)
