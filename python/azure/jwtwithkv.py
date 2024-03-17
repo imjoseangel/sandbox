505bd1531c682f3f0bf71c1a49a88441c95a2e92
imjoseangel <josea.munoz@gmail.com>
2024-03-17 14:01:01 +0100

    add key

--- python/azure/jwtwithkv.py
+++ python/azure/jwtwithkv.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 import jwt
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.keys import KeyClient
-from azure.keyvault.keys.crypto import CryptographyClient
+from azure.core.exceptions import ResourceNotFoundError
+from azure.keyvault.keys.crypto import CryptographyClient, EncryptionAlgorithm
 
 
-VAULT_URL = "https://mykv.vault.azure.net"
+VAULT_URL = "https://nine.vault.azure.net"
 PAYLOAD = {'some': 'payload'}
 
 
@@ -15,12 +17,27 @@ def main():
     credential = DefaultAzureCredential()
     client = KeyClient(vault_url=VAULT_URL, credential=credential)
 
-    key = client.get_key("jwt")
-    crypto_client = CryptographyClient(key, credential=credential)
+    with open('jwtRS256.key.pub', mode='rb') as public_file:
+        public_key = public_file.read()
 
-    encoded_jwt = jwt.encode(PAYLOAD, key=key.key.n, algorithm="HS256")
+    with open('jwtRS256.key', mode='rb') as private_file:
+        private_key = private_file.read()
+
+    try:
+        key = client.get_key("test")
+        crypto_client = CryptographyClient(key, credential=credential)
+    except ResourceNotFoundError as e:
+        print(f'Error {e}')
+        sys.exit(1)
+
+    # encoded_jwt = crypto_client.encrypt(
+    #     EncryptionAlgorithm.rsa_oaep, PAYLOAD)
+    encoded_jwt = jwt.encode(PAYLOAD, key=private_key, algorithm="RS256")
 
     print(f"Encoded JWT: {encoded_jwt}")
+
+    decoded_jwt = jwt.decode(encoded_jwt, public_key, algorithms=["RS256"])
+    print(f"Decoded JWT: {decoded_jwt}")
 
 
 if __name__ == '__main__':
