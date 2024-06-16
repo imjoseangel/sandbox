1f603e637ac996962f653cf62b64fdd21ab78898
imjoseangel <josea.munoz@gmail.com>
2024-06-16 10:13:34 +0200

    add hypothesis from pragmatic

---
+++ python/testing/hipoprag.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from hypothesis import given
+from hypothesis import strategies as some
+
+
+@given(some.lists(some.integers()))
+def test_list_size_is_invariant_across_sorting(a_list):
+    original_length = len(a_list)
+    a_list.sort()
+    assert len(a_list) == original_length
+
+
+@given(some.lists(some.text()))
+def test_sorted_result_is_ordered(a_list):
+    a_list.sort()
+    for i in range(len(a_list) - 1):
+        assert a_list[i] <= a_list[i + 1]
