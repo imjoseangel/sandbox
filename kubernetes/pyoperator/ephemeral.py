3d4dbc7391ae5586e9a4f64c666b9c34b9cd954d
imjoseangel <josea.munoz@gmail.com>
2024-06-29 23:18:26 +0200

    add ephemeral

--- kubernetes/pyoperator/ephemeral.py
+++ kubernetes/pyoperator/ephemeral.py
@@ -2,10 +2,11 @@ import os
 import kopf
 import kubernetes
 import yaml
+from typing import Collection, Mapping, Any
 
 
 @kopf.on.create('ephemeralvolumeclaims')
-def create_fn(spec, name, namespace, logger, **kwargs):
+def create_fn(spec, name, namespace, logger, body, **kwargs):
 
     size = spec.get('size')
     if not size:
@@ -16,6 +17,8 @@ def create_fn(spec, name, namespace, logger, **kwargs):
     text = tmpl.format(name=name, size=size)
     data = yaml.safe_load(text)
 
+    kopf.adopt(data)
+
     api = kubernetes.client.CoreV1Api()
     obj = api.create_namespaced_persistent_volume_claim(
         namespace=namespace,
@@ -23,3 +26,39 @@ def create_fn(spec, name, namespace, logger, **kwargs):
     )
 
     logger.info(f"PVC child is created: {obj}")
+
+    return {'pvc-name': obj.metadata.name}
+
+
+@kopf.on.update('ephemeralvolumeclaims')
+def update_fn(spec, status, namespace, logger, **kwargs):
+
+    size = spec.get('size', None)
+    if not size:
+        raise kopf.PermanentError(f"Size must be set. Got {size!r}.")
+
+    pvc_name = status['create_fn']['pvc-name']
+    pvc_patch = {'spec': {'resources': {'requests': {'storage': size}}}}
+
+    api = kubernetes.client.CoreV1Api()
+    obj = api.patch_namespaced_persistent_volume_claim(
+        namespace=namespace,
+        name=pvc_name,
+        body=pvc_patch,
+    )
+
+    logger.info(f"PVC child is updated: {obj}")
+
+
+@kopf.on.field('ephemeralvolumeclaims', field='metadata.labels')
+def propagate_labels(labels: Mapping[str, str], name: str, namespace: str, **_: Any):
+
+    # TODO: use diff-DSL to flat-walk over labels (including deletions, incl. new [labels] section).
+    pvc_patch = {'metadata': {'labels': dict(labels)}}
+
+    api = kubernetes.client.CoreV1Api()
+    api.patch_namespaced_persistent_volume_claim(
+        namespace=namespace,
+        name=name,
+        body=pvc_patch,
+    )
