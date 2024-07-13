a463f57c3babec81d170224aa5e48e581d605c4c
imjoseangel <josea.munoz@gmail.com>
2024-07-13 19:49:38 +0200

    split

--- terraform/airflow/main.tf
+++ terraform/airflow/main.tf
@@ -1,5 +1,9 @@
 data "external" "fernet_key" {
-  program = ["python", "-c", "from cryptography.fernet import Fernet; import json; FERNET_KEY = Fernet.generate_key().decode(); print(json.dumps({'value': FERNET_KEY}))"]
+  program = [
+    "python",
+    "-c",
+    "from cryptography.fernet import Fernet; import json; FERNET_KEY = Fernet.generate_key().decode(); print(json.dumps({'value': FERNET_KEY}))"
+  ]
 }
 
 resource "random_id" "token" {
