d21c34fa64d731d05ea0bf2e61f559e536f07c77
imjoseangel <josea.munoz@gmail.com>
2024-07-13 21:18:03 +0200

    add special

--- terraform/airflow/main.tf
+++ terraform/airflow/main.tf
@@ -18,7 +18,7 @@ resource "random_id" "token" {
 resource "random_password" "password" {
   length           = 16
   special          = true
-  override_special = "_%@"
+  override_special = "!#$%*-_=+<>:?"
 }
 
 output "fernet_key" {
