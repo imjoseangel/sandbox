1ce0417bc9334b2d957ac3b8483c25e6f2cac2f4
imjoseangel <josea.munoz@gmail.com>
2024-07-13 19:30:31 +0200

    add webtoken

--- terraform/airflow/main.tf
+++ terraform/airflow/main.tf
@@ -2,6 +2,15 @@ data "external" "fernet_key" {
   program = ["python", "-c", "from cryptography.fernet import Fernet; import json; FERNET_KEY = Fernet.generate_key().decode(); print(json.dumps({'value': FERNET_KEY}))"]
 }
 
-output "provisioner" {
+resource "random_id" "token" {
+  byte_length = 16
+}
+
+output "fernet_key" {
   value = data.external.fernet_key.result.value
+}
+
+output "webserver_secret" {
+  value     = random_id.token.hex
+  sensitive = false
 }
