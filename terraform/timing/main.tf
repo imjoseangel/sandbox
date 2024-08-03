0689f4618d5a9461eb723c3ac80f84cb911f639a
imjoseangel <josea.munoz@gmail.com>
2024-08-03 09:02:28 +0200

    add example

---
+++ terraform/timing/main.tf
@@ -0,0 +1,29 @@
+locals {
+  not_before_date = timestamp()
+  expiration_date = timeadd(timestamp(), "4320h")
+}
+
+resource "time_rotating" "main" {
+  rotation_days = 180
+}
+
+resource "random_password" "pass" {
+  length = 24
+  keepers = {
+    time = time_rotating.main.id
+  }
+}
+
+output "locals" {
+  value = {
+    not_before_date = local.not_before_date,
+    expiration_date = local.expiration_date
+  }
+}
+
+output "time_rotating" {
+  value = {
+    not_before_date = time_rotating.main.rfc3339,
+    expiration_date = time_rotating.main.rotation_rfc3339
+  }
+}
