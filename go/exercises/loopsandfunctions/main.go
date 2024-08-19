a54b57d229e35886b83270a99903a78c52eb3cfa
imjoseangel <josea.munoz@gmail.com>
2024-08-19 20:43:19 +0200

    add code

--- go/exercises/loopsandfunctions/main.go
+++ go/exercises/loopsandfunctions/main.go
@@ -10,9 +10,8 @@ func Sqrt(x float64) float64 {
 	result := float64(1)
 	for i := 0; i < 100; i++ {
 		z -= (z*z - x) / (2 * z)
-		fmt.Println(i)
-		fmt.Println(math.Abs(z - result))
 
+		fmt.Println(math.Abs(z - result))
 		if math.Abs(result-z) < 1e-5 {
 			break
 		}
