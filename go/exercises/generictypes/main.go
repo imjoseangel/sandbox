3dc973c1ef06d6aac8c7c4e7bae1622c5f8b6d0f
imjoseangel <josea.munoz@gmail.com>
2024-08-26 21:42:31 +0200

    remove var

--- go/exercises/generictypes/main.go
+++ go/exercises/generictypes/main.go
@@ -11,8 +11,8 @@ type List[T any] struct {
 
 func main() {
 
-	var l1 List[string]
-	l1 = List[string]{val: "hello", next: &List[string]{val: "world", next: nil}}
+	// Directly initialize the List with its desired structure instead of assigning it step by step
+	var l1 = &List[string]{val: "hello", next: &List[string]{val: "world", next: nil}}
 
 	fmt.Println(l1.val, l1.next.val)
 
