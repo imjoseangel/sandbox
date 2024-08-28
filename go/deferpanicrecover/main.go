828c501c2bbb25abaea45e61c4a8e97555210a8d
imjoseangel <josea.munoz@gmail.com>
2024-08-28 19:00:45 +0200

    add panicrecover

---
+++ go/deferpanicrecover/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func c() (i int) {
+	defer func() { i++ }()
+	return 2
+}
+
+func f() {
+	deferfunc() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in f", r)
+		}
+	}()
+	fmt.Println("Calling g.")
+	g(0)
+	fmt.Println("Returned normally from g.")
+}
+
+func g(i int) {
+	if i > 3 {
+		fmt.Println("Panicking!")
+		panic(fmt.Sprintf("%v", i))
+	}
+	defer fmt.Println("Defer in g", i)
+	fmt.Println("Printing in g", i)
+	g(i + 1)
+}
+
+func main() {
+	c := c()
+	fmt.Println(c)
+
+	f()
+	fmt.Println("Returned normally from f.")
+}
