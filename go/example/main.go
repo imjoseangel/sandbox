34b8de959add881703cca0db524f5e4cfdb52e45
imjoseangel <josea.munoz@gmail.com>
2024-09-21 23:41:53 +0200

    add example

---
+++ go/example/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	// Create a new HTTP request
+	req, err := http.NewRequest("GET", "https://example.com", nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(string(body))
+}
