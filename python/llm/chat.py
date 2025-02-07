7189c74d68446fd06a11eff6784300923b9f210f
imjoseangel <josea.munoz@gmail.com>
2025-02-08 00:01:12 +0100

    add history

--- python/llm/chat.py
+++ python/llm/chat.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, List
 from pydantic import BaseModel, Field
 import requests
 
@@ -33,47 +33,49 @@ def wikipedia_text_exporter(article: str) -> dict[str, str]:
     return {"text": result}
 
 
-chat_history: list[Union[HumanMessage, SystemMessage]] = []
+class ChatHistoryManager:
+    def __init__(self):
+        self.chat_history: List[Union[HumanMessage, SystemMessage]] = [
+            SystemMessage(content="You're a helpful assistant.")]
+
+    def append_chat_history(self, user_input, response):
+        self.chat_history.append(HumanMessage(content=user_input))
+        self.chat_history.append(SystemMessage(content=response))
+
+    def trim_chat_history(self):
+        self.chat_history = trim_messages(
+            self.chat_history,
+            token_counter=len,  # len will simply count the number of messages rather than tokens
+            max_tokens=5,  # allow up to 5 messages
+            strategy="last",
+            start_on="human",
+            include_system=True,
+            allow_partial=False,
+        )
 
 
-def append_chat_history(user_input, response):
-    chat_history.append(HumanMessage(content=user_input))
-    chat_history.append(SystemMessage(content=response))
-
-
-def invoke(user_input):
-    global chat_history  # Declare chat_history as global here
+def invoke(user_input, history_manager: ChatHistoryManager):
     msg = {
         "input": user_input,
-        "chat_history": chat_history,
+        "chat_history": history_manager.chat_history,
     }
     print(f"Input: {msg}")
 
     response = agent_executor.invoke(msg)
     print(f"Response: {response}")
 
-    append_chat_history(response["input"], response["output"])
-
-    # Trim chat history to the last 5 messages
-    chat_history = trim_messages(
-        chat_history,
-        token_counter=len,  # len will simply count the number of messages rather than tokens
-        max_tokens=5,  # allow up to 5 messages
-        strategy="last",
-        start_on="human",
-        include_system=True,
-        allow_partial=False,
-    )
-    print(f"History: {chat_history}")
+    history_manager.append_chat_history(user_input, response["output"])
+    history_manager.trim_chat_history()
+    print(f"History: {history_manager.chat_history}")
 
 
 tools = [wikipedia_text_exporter]
 prompt = hub.pull("hwchase17/react-chat")
-chat_history = [SystemMessage(content="You're a helpful assistant.")]
 llm = Ollama(model="gemma2:9b")
 
 agent = create_react_agent(llm, tools, prompt)
 agent_executor = AgentExecutor(
     agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
 
-invoke("What is the capital of Germany? Do not use a tool.")
+history = ChatHistoryManager()
+invoke("What is the capital of Germany? Do not use a tool.", history)
