9520df3f347850b9035a450945594ff0f66febbd
imjoseangel <josea.munoz@gmail.com>
2025-02-12 23:39:58 +0100

    add react

---
+++ python/llm/workflows/react.py
@@ -0,0 +1,25 @@
+
+from llama_index.core.agent import ReActAgent
+from llama_index.llms.ollama import Ollama
+from llama_index.core.tools import FunctionTool
+
+
+def multiply(a: int, b: int) -> int:
+    """Multiply two integers and returns the result integer"""
+    return a * b
+
+
+multiply_tool = FunctionTool.from_defaults(fn=multiply)
+
+
+def add(a: int, b: int) -> int:
+    """Add two integers and returns the result integer"""
+    return a + b
+
+
+add_tool = FunctionTool.from_defaults(fn=add)
+
+llm = Ollama(model="gemma2:latest", request_timeout=120.0)
+agent = ReActAgent.from_tools([multiply_tool, add_tool], llm=llm, verbose=True)
+
+response = agent.chat("What is 20+(2*4)? Calculate step by step ")
