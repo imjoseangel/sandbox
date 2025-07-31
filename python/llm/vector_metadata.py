3b172939c15fdd7a66a162434edf8d6b68f4e188
joseangel <joseangel.munoz@zurich.com>
2025-07-31 19:19:49 +0200

    add meta

---
+++ python/llm/vector_metadata.py
@@ -0,0 +1,24 @@
+from llama_index.core import Document
+from llama_index.core.schema import MetadataMode
+
+document = Document(
+    text="This is a super-customized document",
+    metadata={
+        "file_name": "super_secret_document.txt",
+        "category": "finance",
+        "author": "LlamaIndex",
+    },
+    excluded_llm_metadata_keys=["file_name"],
+    metadata_seperator="::",
+    metadata_template="{key}=>{value}",
+    text_template="Metadata: {metadata_str}\n-----\nContent: {content}",
+)
+
+print(
+    "The LLM sees this: \n",
+    document.get_content(metadata_mode=MetadataMode.LLM),
+)
+print(
+    "The Embedding model sees this: \n",
+    document.get_content(metadata_mode=MetadataMode.EMBED),
+)
