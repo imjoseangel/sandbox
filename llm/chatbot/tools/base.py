78252edc50a8cbf179b02a257cb3515843a605e6
imjoseangel <josea.munoz@gmail.com>
2025-08-02 23:40:17 +0200

    add base

---
+++ llm/chatbot/tools/base.py
@@ -0,0 +1,15 @@
+from llama_index.core.tools.tool_spec.base import BaseToolSpec
+
+
+class MathTool(BaseToolSpec):
+    """MathTool is a base class for mathematical operations.
+    It provides methods to perform basic arithmetic operations like sum and subtract.
+    """
+
+    spec_functions = ["sum", "subtract"]
+
+    def sum(self, a: float, b: float) -> float:
+        return a + b
+
+    def subtract(self, a: float, b: float) -> float:
+        return a - b
