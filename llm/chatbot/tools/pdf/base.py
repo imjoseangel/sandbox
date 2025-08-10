0d6debbb449196c0b3ecefa5851afbe90e42695e
imjoseangel <josea.munoz@gmail.com>
2025-08-10 20:38:25 +0100

    add logger

--- llm/chatbot/tools/pdf/base.py
+++ llm/chatbot/tools/pdf/base.py
@@ -21,10 +21,6 @@ class PDFTool(BaseToolSpec):
 
         # --- Start Logging Configuration ---
         self.logger = logging.getLogger(__name__)
-        self.formatter = logging.Formatter(
-            "%(asctime)s | %(levelname)-8s | %(name)s:%(funcName)s:%(lineno)d - %(message)s",
-            datefmt="%Y-%m-%d %H:%M:%S",
-        )
 
     def _load_document(self):
         """Load the PDF document and create indexes."""
